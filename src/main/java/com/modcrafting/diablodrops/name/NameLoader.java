c5824bb3be4f1609112b971ec4d8600018d22f20
Richard Harrah <topplethenunnery@gmail.com>
2013-05-13 09:20:27 -0400

    updating to fix some try/catch mistakes

--- src/main/java/com/modcrafting/diablodrops/name/NameLoader.java
+++ src/main/java/com/modcrafting/diablodrops/name/NameLoader.java
@@ -8,75 +8,63 @@ package com.modcrafting.diablodrops.name;
 
 import com.conventnunnery.plugins.mythicdrops.MythicDrops;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.InputStream;
+import java.io.*;
 import java.util.List;
 
 public class NameLoader {
 
-	private final File dataFolder;
-	private final MythicDrops plugin;
+    private final File dataFolder;
+    private final MythicDrops plugin;
 
-	public NameLoader(final MythicDrops instance) {
-		plugin = instance;
-		dataFolder = instance.getDataFolder();
-	}
+    public NameLoader(final MythicDrops instance) {
+        plugin = instance;
+        dataFolder = instance.getDataFolder();
+    }
 
-	/**
-	 * @return the plugin
-	 */
-	public MythicDrops getPlugin() {
-		return plugin;
-	}
+    /**
+     * @return the plugin
+     */
+    public MythicDrops getPlugin() {
+        return plugin;
+    }
 
-	public void loadFile(final List<String> l, final String name) {
-		try {
-			BufferedReader list = new BufferedReader(new FileReader(new File(
-					dataFolder, name)));
-			String p;
-			while ((p = list.readLine()) != null) {
-				if (!p.contains("#") && p.length() > 0) {
-					l.add(p);
-				}
-			}
-			list.close();
-		} catch (Exception ignored) {
-			getPlugin().getDebug().debug("Could not load " + name);
-		}
-	}
+    public void loadFile(final List<String> l, final String name) throws IOException {
+        BufferedReader list = new BufferedReader(new FileReader(new File(
+                dataFolder, name)));
+        String p;
+        while ((p = list.readLine()) != null) {
+            if (!p.contains("#") && p.length() > 0) {
+                l.add(p);
+            }
+        }
+        list.close();
+    }
 
-	/**
-	 * Creates a file with given name
-	 *
-	 * @param name Name of the file to write
-	 */
-	public void writeDefault(final String name, boolean overwrite) {
-		File actual = new File(dataFolder, name);
-		if (name.contains(".jar")) {
-			actual = new File(dataFolder.getParent(), name);
-		}
-		if (!actual.exists() || overwrite) {
-			if (!actual.getParentFile().exists() && !actual.getParentFile().mkdirs()) {
-				return;
-			}
-			try {
-				InputStream input = this.getClass().getResourceAsStream(
-						"/" + name);
-				FileOutputStream output = new FileOutputStream(actual, false);
-				byte[] buf = new byte[1024];
-				int length;
-				while ((length = input.read(buf)) > 0) {
-					output.write(buf, 0, length);
-				}
-				output.close();
-				input.close();
-			} catch (Exception ignored) {
-				getPlugin().getDebug().debug("Could not write " + name);
-			}
-		}
-	}
+    /**
+     * Creates a file with given name
+     *
+     * @param name Name of the file to write
+     */
+    public void writeDefault(final String name, boolean overwrite) throws IOException {
+        File actual = new File(dataFolder, name);
+        if (name.contains(".jar")) {
+            actual = new File(dataFolder.getParent(), name);
+        }
+        if (!actual.exists() || overwrite) {
+            if (!actual.getParentFile().exists() && !actual.getParentFile().mkdirs()) {
+                return;
+            }
+            InputStream input = this.getClass().getResourceAsStream(
+                    "/" + name);
+            FileOutputStream output = new FileOutputStream(actual, false);
+            byte[] buf = new byte[1024];
+            int length;
+            while ((length = input.read(buf)) > 0) {
+                output.write(buf, 0, length);
+            }
+            output.close();
+            input.close();
+        }
+    }
 
 }
