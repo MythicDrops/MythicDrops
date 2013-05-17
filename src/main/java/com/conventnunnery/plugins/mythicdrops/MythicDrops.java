57f25490dd12ac5d394996465f145c269c6efbab
Richard Harrah <topplethenunnery@gmail.com>
2013-05-16 20:25:40 -0400

    adding repaircosts

--- src/main/java/com/conventnunnery/plugins/mythicdrops/MythicDrops.java
+++ src/main/java/com/conventnunnery/plugins/mythicdrops/MythicDrops.java
@@ -11,19 +11,13 @@
 package com.conventnunnery.plugins.mythicdrops;
 
 import com.conventnunnery.plugins.conventlib.debug.Debugger;
+import com.conventnunnery.plugins.mythicdrops.builders.RepairCostBuilder;
 import com.conventnunnery.plugins.mythicdrops.builders.SocketGemBuilder;
 import com.conventnunnery.plugins.mythicdrops.command.MythicDropsCommand;
 import com.conventnunnery.plugins.mythicdrops.listeners.EntityListener;
 import com.conventnunnery.plugins.mythicdrops.listeners.ItemListener;
 import com.conventnunnery.plugins.mythicdrops.listeners.RepairListener;
-import com.conventnunnery.plugins.mythicdrops.managers.ConfigurationManager;
-import com.conventnunnery.plugins.mythicdrops.managers.DropManager;
-import com.conventnunnery.plugins.mythicdrops.managers.EntityManager;
-import com.conventnunnery.plugins.mythicdrops.managers.ItemManager;
-import com.conventnunnery.plugins.mythicdrops.managers.LanguageManager;
-import com.conventnunnery.plugins.mythicdrops.managers.NameManager;
-import com.conventnunnery.plugins.mythicdrops.managers.SocketGemManager;
-import com.conventnunnery.plugins.mythicdrops.managers.TierManager;
+import com.conventnunnery.plugins.mythicdrops.managers.*;
 import com.modcrafting.diablodrops.builders.CustomBuilder;
 import com.modcrafting.diablodrops.builders.TierBuilder;
 import net.h3lix.updater.Updater;
@@ -36,160 +30,173 @@ import java.util.Random;
 
 public class MythicDrops extends JavaPlugin implements Listener {
 
-	private static MythicDrops instance;
-	private ConfigurationManager configurationManager;
-	private PluginSettings pluginSettings;
-	private TierManager tierManager;
-	private NameManager nameManager;
-	private ItemManager itemManager;
-	private DropManager dropManager;
-	private EntityManager entityManager;
-	private SocketGemManager socketGemManager;
-	private TierBuilder tierBuilder;
-	private CustomBuilder customBuilder;
-	private SocketGemBuilder socketGemBuilder;
-	private Debugger debug;
-	private Random random = new Random();
-	private LanguageManager languageManager;
+    private static MythicDrops instance;
+    private ConfigurationManager configurationManager;
+    private PluginSettings pluginSettings;
+    private TierManager tierManager;
+    private NameManager nameManager;
+    private ItemManager itemManager;
+    private DropManager dropManager;
+    private EntityManager entityManager;
+    private SocketGemManager socketGemManager;
+    private RepairManager repairManager;
+    private TierBuilder tierBuilder;
+    private CustomBuilder customBuilder;
+    private SocketGemBuilder socketGemBuilder;
+    private RepairCostBuilder repairCostBuilder;
+    private Debugger debug;
+    private Random random = new Random();
+    private LanguageManager languageManager;
 
-	public static MythicDrops getInstance() {
-		return instance;
-	}
+    public static MythicDrops getInstance() {
+        return instance;
+    }
 
-	public LanguageManager getLanguageManager() {
-		return languageManager;
-	}
+    public RepairCostBuilder getRepairCostBuilder() {
+        return repairCostBuilder;
+    }
 
-	public SocketGemBuilder getSocketGemBuilder() {
-		return socketGemBuilder;
-	}
+    public RepairManager getRepairManager() {
+        return repairManager;
+    }
 
-	public SocketGemManager getSocketGemManager() {
-		return socketGemManager;
-	}
+    public LanguageManager getLanguageManager() {
+        return languageManager;
+    }
 
-	public TierBuilder getTierBuilder() {
-		return tierBuilder;
-	}
+    public SocketGemBuilder getSocketGemBuilder() {
+        return socketGemBuilder;
+    }
 
-	public CustomBuilder getCustomBuilder() {
-		return customBuilder;
-	}
+    public SocketGemManager getSocketGemManager() {
+        return socketGemManager;
+    }
 
-	public Random getRandom() {
-		return random;
-	}
+    public TierBuilder getTierBuilder() {
+        return tierBuilder;
+    }
 
-	public ConfigurationManager getConfigurationManager() {
-		return configurationManager;
-	}
+    public CustomBuilder getCustomBuilder() {
+        return customBuilder;
+    }
 
-	/**
-	 * @return the debug
-	 */
-	public Debugger getDebug() {
-		return debug;
-	}
+    public Random getRandom() {
+        return random;
+    }
 
-	/**
-	 * @return the dropManager
-	 */
-	public DropManager getDropManager() {
-		return dropManager;
-	}
+    public ConfigurationManager getConfigurationManager() {
+        return configurationManager;
+    }
 
-	/**
-	 * @return the entityManager
-	 */
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
+    /**
+     * @return the debug
+     */
+    public Debugger getDebug() {
+        return debug;
+    }
 
-	/**
-	 * @return the itemManager
-	 */
-	public ItemManager getItemManager() {
-		return itemManager;
-	}
+    /**
+     * @return the dropManager
+     */
+    public DropManager getDropManager() {
+        return dropManager;
+    }
 
-	/**
-	 * @return the nameManager
-	 */
-	public NameManager getNameManager() {
-		return nameManager;
-	}
+    /**
+     * @return the entityManager
+     */
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
 
-	public PluginSettings getPluginSettings() {
-		return pluginSettings;
-	}
+    /**
+     * @return the itemManager
+     */
+    public ItemManager getItemManager() {
+        return itemManager;
+    }
 
-	/**
-	 * @return the tierManager
-	 */
-	public TierManager getTierManager() {
-		return tierManager;
-	}
+    /**
+     * @return the nameManager
+     */
+    public NameManager getNameManager() {
+        return nameManager;
+    }
 
-	@Override
-	public void onEnable() {
-		instance = this;
-		debug = new Debugger(this);
-		configurationManager = new ConfigurationManager(this);
-		pluginSettings = new PluginSettings(this);
-		pluginSettings.loadPluginSettings();
-		languageManager = new LanguageManager(this);
-		tierManager = new TierManager(this);
-		nameManager = new NameManager(this);
-		itemManager = new ItemManager(this);
-		dropManager = new DropManager(this);
-		entityManager = new EntityManager(this);
-		socketGemManager = new SocketGemManager(this);
-		tierBuilder = new TierBuilder(this);
-		customBuilder = new CustomBuilder(this);
-		socketGemBuilder = new SocketGemBuilder(this);
-		tierBuilder.build();
-		customBuilder.build();
-		socketGemBuilder.build();
-		if (pluginSettings.isDebugOnStartup()) {
-			pluginSettings.debugSettings();
-			getNameManager().debugPrefixesAndSuffixes();
-			getTierManager().debugTiers();
-			getDropManager().debugCustomItems();
-			getSocketGemManager().debugSocketGems();
-		}
-		getCommand("mythicdrops").setExecutor(new MythicDropsCommand(this));
-		getServer().getPluginManager().registerEvents(new EntityListener(this),
-				this);
-		getServer().getPluginManager().registerEvents(new ItemListener(this), this);
+    public PluginSettings getPluginSettings() {
+        return pluginSettings;
+    }
+
+    /**
+     * @return the tierManager
+     */
+    public TierManager getTierManager() {
+        return tierManager;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        debug = new Debugger(this);
+        configurationManager = new ConfigurationManager(this);
+        pluginSettings = new PluginSettings(this);
+        pluginSettings.loadPluginSettings();
+        languageManager = new LanguageManager(this);
+        tierManager = new TierManager(this);
+        nameManager = new NameManager(this);
+        itemManager = new ItemManager(this);
+        dropManager = new DropManager(this);
+        entityManager = new EntityManager(this);
+        socketGemManager = new SocketGemManager(this);
+        repairManager = new RepairManager(this);
+        tierBuilder = new TierBuilder(this);
+        customBuilder = new CustomBuilder(this);
+        socketGemBuilder = new SocketGemBuilder(this);
+        repairCostBuilder = new RepairCostBuilder(this);
+        tierBuilder.build();
+        customBuilder.build();
+        socketGemBuilder.build();
+        repairCostBuilder.build();
+        if (pluginSettings.isDebugOnStartup()) {
+            pluginSettings.debugSettings();
+            getNameManager().debugPrefixesAndSuffixes();
+            getTierManager().debugTiers();
+            getDropManager().debugCustomItems();
+            getSocketGemManager().debugSocketGems();
+        }
+        getCommand("mythicdrops").setExecutor(new MythicDropsCommand(this));
+        getServer().getPluginManager().registerEvents(new EntityListener(this),
+                this);
+        getServer().getPluginManager().registerEvents(new ItemListener(this), this);
         getServer().getPluginManager().registerEvents(new RepairListener(this), this);
-		if (getPluginSettings().isAutomaticUpdate()) {
-			new Updater(this, "mythic", this.getFile(),
-					Updater.UpdateType.DEFAULT, false);
-		}
-		startStatistics();
-	}
+        if (getPluginSettings().isAutomaticUpdate()) {
+            new Updater(this, "mythic", this.getFile(),
+                    Updater.UpdateType.DEFAULT, false);
+        }
+        startStatistics();
+    }
 
-	private void startStatistics() {
-		try {
-			Metrics metrics = new Metrics(this);
-			Metrics.Graph graph = metrics.createGraph("Tiers Used");
-			graph.addPlotter(new Metrics.Plotter() {
-				@Override
-				public int getValue() {
-					return getTierManager().getTiers().size();
-				}
-			});
-			Metrics.Graph graph2 = metrics.createGraph("Custom Items Created");
-			graph2.addPlotter(new Metrics.Plotter() {
-				@Override
-				public int getValue() {
-					return getDropManager().getCustomItems().size();
-				}
-			});
-			metrics.start();
-		} catch (IOException e) {
-			getLogger().info("Could not begin Metrics.");
-		}
-	}
+    private void startStatistics() {
+        try {
+            Metrics metrics = new Metrics(this);
+            Metrics.Graph graph = metrics.createGraph("Tiers Used");
+            graph.addPlotter(new Metrics.Plotter() {
+                @Override
+                public int getValue() {
+                    return getTierManager().getTiers().size();
+                }
+            });
+            Metrics.Graph graph2 = metrics.createGraph("Custom Items Created");
+            graph2.addPlotter(new Metrics.Plotter() {
+                @Override
+                public int getValue() {
+                    return getDropManager().getCustomItems().size();
+                }
+            });
+            metrics.start();
+        } catch (IOException e) {
+            getLogger().info("Could not begin Metrics.");
+        }
+    }
 
 }
---
+++ src/main/java/com/conventnunnery/plugins/mythicdrops/managers/RepairManager.java
@@ -0,0 +1,86 @@
+package com.conventnunnery.plugins.mythicdrops.managers;
+
+import com.conventnunnery.plugins.mythicdrops.MythicDrops;
+import com.conventnunnery.plugins.mythicdrops.objects.RepairCost;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RepairManager {
+    private final MythicDrops plugin;
+    private Set<RepairCost> repairCostSet;
+
+    public RepairManager(MythicDrops plugin) {
+        this.plugin = plugin;
+        repairCostSet = new HashSet<RepairCost>();
+    }
+
+    public Set<RepairCost> getRepairCostSet() {
+        return repairCostSet;
+    }
+
+    public RepairCost getRepairCost(MaterialData materialData) {
+        MaterialData backup = new MaterialData(materialData.getItemType(), (byte) 0);
+        for (RepairCost repairCost : repairCostSet) {
+            if (repairCost.getMaterialData().equals(materialData) || repairCost.getMaterialData().equals(backup)) {
+                return repairCost;
+            }
+        }
+        return null;
+    }
+
+    public ItemStack repairItemStack(ItemStack itemStack, Inventory inventory) {
+        if (itemStack == null) {
+            return itemStack;
+        }
+        ItemStack repaired = itemStack.clone();
+        RepairCost repairCost = getRepairCost(repaired.getData());
+        if (repairCost == null) {
+            return repaired;
+        }
+        if (!inventory.containsAtLeast(repairCost.getMaterialData().toItemStack(1), repairCost.getAmountRequired())) {
+            return repaired;
+        }
+        int taken = 0;
+        ItemStack slot;
+        while (taken < repairCost.getAmountRequired() &&
+                inventory.containsAtLeast(repairCost.getMaterialData().toItemStack(1),
+                        repairCost.getAmountRequired() - taken)) {
+            slot = inventory.getItem(inventory.first(repairCost.getMaterialData().getItemType()));
+            if (slot.getAmount() >= repairCost.getAmountRequired()) {
+                slot.setAmount(slot.getAmount() - repairCost.getAmountRequired());
+                taken += repairCost.getAmountRequired();
+            } else {
+                taken += slot.getAmount();
+                slot.setAmount(0);
+            }
+        }
+        if (taken < repairCost.getAmountRequired()) {
+            for (HumanEntity humanEntity : inventory.getViewers()) {
+                if (humanEntity instanceof Player) {
+                    ((Player) humanEntity).updateInventory();
+                }
+            }
+            return repaired;
+        }
+        short currentDurability = repaired.getDurability();
+        short newDurability = (short) (currentDurability - (repaired.getType().getMaxDurability()
+                        * repairCost.getPercentageRestored()));
+        repaired.setDurability(newDurability);
+        for (HumanEntity humanEntity : inventory.getViewers()) {
+            if (humanEntity instanceof Player) {
+                ((Player) humanEntity).updateInventory();
+            }
+        }
+        return repaired;
+     }
+
+    public MythicDrops getPlugin() {
+        return plugin;
+    }
+}
