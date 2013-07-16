4c84a16cec2e08cf21e6d2ddd2e12199e5130bab
Richard Harrah <topplethenunnery@gmail.com>
2013-07-16 15:33:43 -0400

    loading MythicItems

---
+++ src/main/java/com/conventnunnery/plugins/mythicdrops/loaders/MythicCustomItemLoader.java
@@ -0,0 +1,72 @@
+package com.conventnunnery.plugins.mythicdrops.loaders;
+
+import com.conventnunnery.libraries.config.ConventYamlConfiguration;
+import com.conventnunnery.plugins.mythicdrops.MythicDrops;
+import com.conventnunnery.plugins.mythicdrops.api.items.CustomItem;
+import com.conventnunnery.plugins.mythicdrops.api.utils.MythicLoader;
+import com.conventnunnery.plugins.mythicdrops.configuration.MythicConfigurationFile;
+import com.conventnunnery.plugins.mythicdrops.items.MythicCustomItem;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.material.MaterialData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MythicCustomItemLoader implements MythicLoader {
+    private final MythicDrops plugin;
+
+    public MythicCustomItemLoader(final MythicDrops plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void load() {
+        getPlugin().getCustomItemManager().getCustomItems().clear();
+        ConventYamlConfiguration configuration = getPlugin().getConfigurationManager().getConfiguration
+                (MythicConfigurationFile.CUSTOM_ITEMS);
+        for (String key : configuration.getKeys(false)) {
+            if (!configuration.isConfigurationSection(key)) {
+                continue;
+            }
+            ConfigurationSection cs = configuration.getConfigurationSection(key);
+            MythicCustomItem mythicCustomItem = new MythicCustomItem();
+            mythicCustomItem.setName(key);
+            mythicCustomItem.setDisplayName(cs.getString("displayName", mythicCustomItem.getName()));
+            mythicCustomItem.setLore(cs.getStringList("lore"));
+            mythicCustomItem.setMaterialData(new MaterialData(cs.getInt("materialID", 0),
+                    (byte) cs.getInt("materialData", 0)));
+            if (mythicCustomItem.getMaterialData().getItemTypeId() == 0) {
+                mythicCustomItem = null;
+                continue;
+            }
+            Map<Enchantment, Integer> map = new HashMap<Enchantment, Integer>();
+            if (cs.isConfigurationSection("enchantments")) {
+                ConfigurationSection enchCS = cs
+                        .getConfigurationSection("enchantments");
+                for (String s2 : enchCS.getKeys(false)) {
+                    Enchantment ench = null;
+                    for (Enchantment ec : Enchantment.values()) {
+                        if (ec.getName().equalsIgnoreCase(s2)) {
+                            ench = ec;
+                            break;
+                        }
+                    }
+                    if (ench == null) {
+                        continue;
+                    }
+                    int level = enchCS.getInt(s2);
+                    map.put(ench, level);
+                }
+            }
+            mythicCustomItem.setEnchantments(map);
+            mythicCustomItem.setChanceToBeGivenToAMonster(cs.getDouble("chanceToBeGivenToAMonster", 1.0));
+            mythicCustomItem.setChanceToDropOnMonsterDeath(cs.getDouble("chanceToDropOnMonsterDeath", 1.0));
+            getPlugin().getCustomItemManager().getCustomItems().add(mythicCustomItem);
+        }
+    }
+
+    public MythicDrops getPlugin() {
+        return plugin;
+    }
+}
