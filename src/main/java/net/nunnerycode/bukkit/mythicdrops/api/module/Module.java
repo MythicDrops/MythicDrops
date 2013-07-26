b12e994fe14bf9471cad9efe97dd2f923625e510
Richard Harrah <topplethenunnery@gmail.com>
2013-07-25 23:13:56 -0400

    preparing for module system

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/api/module/Module.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/api/module/Module.java
@@ -1,6 +1,10 @@
 package net.nunnerycode.bukkit.mythicdrops.api.module;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 
 public interface Module extends Listener {
+
+    FileConfiguration getFileConfiguration();
+
 }
