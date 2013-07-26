fc693db0b748a51313c7a34489af637dd67ecfce
Richard Harrah <topplethenunnery@gmail.com>
2013-07-26 08:12:27 -0400

    changing module to a class and adding CreatureSpawnModule

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/api/module/Module.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/api/module/Module.java
@@ -1,10 +1,24 @@
 package net.nunnerycode.bukkit.mythicdrops.api.module;
 
+import com.conventnunnery.libraries.config.ConventYamlConfiguration;
+import net.nunnerycode.bukkit.mythicdrops.MythicDrops;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 
-public interface Module extends Listener {
+import java.io.File;
 
-    FileConfiguration getFileConfiguration();
+public class Module implements Listener {
+
+    public FileConfiguration getFileConfiguration() {
+        return fileConfiguration;
+    }
+
+    private FileConfiguration fileConfiguration;
+
+    public Module() {
+        fileConfiguration = ConventYamlConfiguration.loadConfiguration(new File(MythicDrops.instance.getDataFolder(),
+                "/modules/" + this.getClass().getName() + ".yml"));
+    }
+
 
 }
---
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/module/CreatureSpawnModule.java
@@ -0,0 +1,7 @@
+package net.nunnerycode.bukkit.mythicdrops.module;
+
+import net.nunnerycode.bukkit.mythicdrops.api.module.Module;
+
+public final class CreatureSpawnModule extends Module {
+
+}
