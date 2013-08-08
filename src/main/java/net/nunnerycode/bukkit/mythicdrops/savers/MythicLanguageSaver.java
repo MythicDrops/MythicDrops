828ef863c4c4161a710d89c5551e123029755cbf
Richard Harrah <topplethenunnery@gmail.com>
2013-08-08 00:13:58 -0400

    adding three of the four eventual savers

---
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/savers/MythicLanguageSaver.java
@@ -0,0 +1,32 @@
+package net.nunnerycode.bukkit.mythicdrops.savers;
+
+import com.conventnunnery.libraries.config.ConventConfiguration;
+import net.nunnerycode.bukkit.mythicdrops.MythicDrops;
+import net.nunnerycode.bukkit.mythicdrops.api.utils.MythicSaver;
+
+import java.util.Map;
+
+public class MythicLanguageSaver implements MythicSaver {
+
+	private MythicDrops plugin;
+
+	public MythicLanguageSaver(MythicDrops plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public void save() {
+		ConventConfiguration c = getPlugin().getConventConfigurationGroup().getConventConfiguration("language.yml");
+		if (c == null) {
+			return;
+		}
+		for (Map.Entry<String, String> key : getPlugin().getLanguageManager().getMessages().entrySet()) {
+			c.getFileConfiguration().set(key.getKey(), key.getValue());
+		}
+		c.save();
+	}
+
+	public MythicDrops getPlugin() {
+		return plugin;
+	}
+}
