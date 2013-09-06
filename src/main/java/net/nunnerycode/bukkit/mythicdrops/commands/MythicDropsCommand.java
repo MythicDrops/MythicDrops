bb69f1d90f5b4d851927a1a47ccb9a4127560c26
Richard Harrah <topplethenunnery@gmail.com>
2013-09-06 01:47:07 -0400

    adding custom command

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/commands/MythicDropsCommand.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/commands/MythicDropsCommand.java
@@ -1,9 +1,11 @@
 package net.nunnerycode.bukkit.mythicdrops.commands;
 
 import net.nunnerycode.bukkit.mythicdrops.MythicDrops;
+import net.nunnerycode.bukkit.mythicdrops.api.items.CustomItem;
 import net.nunnerycode.bukkit.mythicdrops.api.items.ItemGenerationReason;
 import net.nunnerycode.bukkit.mythicdrops.api.tiers.Tier;
 import net.nunnerycode.bukkit.mythicdrops.utils.ItemStackUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -117,6 +119,72 @@ public class MythicDropsCommand {
 		getPlugin().getLanguageManager().sendMessage(player, "command.give-random-receiver",
 				new String[][]{{"%amount%", String.valueOf(amountGiven)}});
 		getPlugin().getLanguageManager().sendMessage(sender, "command.give-random-sender", new String[][]{{"%amount%",
+				String.valueOf(amountGiven)}, {"%receiver%", player.getName()}});
+	}
+
+	@Command(identifier = "mythicdrops give", description = "Gives in MythicDrops items",
+			permissions = "mythicdrops.command.give")
+	@Flags(identifier = {"a", "c", "mind", "maxd"}, description = {"Amount to spawn", "Custom Item to spawn",
+			"Minimum durability", "Maximum durability"})
+	public void customSubcommand(CommandSender sender, @Arg(name = "player") String playerName, @Arg(name = "amount",
+			def = "1")
+	@FlagArg("a") int amount, @Arg(name = "tier", def = "*") @FlagArg("c") String itemName,
+								 @Arg(name = "mindurability", def = "1.0", verifiers = "min[0.0]|max[1.0]") @FlagArg
+										 ("mind") double minDura,
+								 @Arg(name = "maxdurability", def = "1.0", verifiers = "min[0.0]|max[1.0]") @FlagArg
+										 ("maxd") double maxDura) {
+		if (!sender.hasPermission("mythicdrops.command.custom")) {
+			getPlugin().getLanguageManager().sendMessage(sender, "command.no-access");
+			return;
+		}
+		Player player;
+		if (playerName.equalsIgnoreCase("self")) {
+			if (sender instanceof Player) {
+				player = (Player) sender;
+			} else {
+				getPlugin().getLanguageManager().sendMessage(sender, "command.no-access");
+				return;
+			}
+		} else {
+			player = Bukkit.getPlayer(playerName);
+		}
+		if (player == null) {
+			getPlugin().getLanguageManager().sendMessage(sender, "command.player-does-not-exist");
+			return;
+		}
+		CustomItem customItem = null;
+		if (!itemName.equalsIgnoreCase("*")) {
+			try {
+				customItem = getPlugin().getCustomItemManager().getCustomItemFromName(itemName);
+				if (customItem == null) {
+					customItem = getPlugin().getCustomItemManager().getCustomItemFromDisplayName(itemName);
+				}
+			} catch (NullPointerException e) {
+				getPlugin().getLanguageManager().sendMessage(sender, "command.custom-item-does-not-exist");
+				return;
+			}
+		}
+		int amountGiven = 0;
+		for (int i = 0; i < amount; i++) {
+			try {
+				ItemStack itemStack;
+				if (customItem == null) {
+					itemStack = getPlugin().getDropManager().generateItemStackFromCustomItem(getPlugin()
+							.getCustomItemManager().getRandomCustomItemWithChance(), ItemGenerationReason.COMMAND);
+				} else {
+					itemStack = getPlugin().getDropManager().generateItemStackFromCustomItem(customItem,
+							ItemGenerationReason.COMMAND);
+				}
+				itemStack.setDurability(ItemStackUtils.getDurabilityForMaterial(itemStack.getType(), minDura,
+						maxDura));
+				player.getInventory().addItem(itemStack);
+				amountGiven++;
+			} catch (Exception ignored) {
+			}
+		}
+		getPlugin().getLanguageManager().sendMessage(player, "command.give-custom-receiver",
+				new String[][]{{"%amount%", String.valueOf(amountGiven)}});
+		getPlugin().getLanguageManager().sendMessage(sender, "command.give-custom-sender", new String[][]{{"%amount%",
 				String.valueOf(amountGiven)}, {"%receiver%", player.getName()}});
 	}
 
