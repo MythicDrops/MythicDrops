52f014b0aba06fb912df913d5f66bdc7e01cbfac
Richard Harrah <topplethenunnery@gmail.com>
2013-09-07 04:34:10 -0400

    bumping up to RC4 due to event change

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/events/RandomItemGenerationEvent.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/events/RandomItemGenerationEvent.java
@@ -6,27 +6,39 @@ import net.nunnerycode.bukkit.mythicdrops.api.tiers.Tier;
 import org.bukkit.inventory.ItemStack;
 
 public class RandomItemGenerationEvent extends MythicDropsEvent implements ItemGenerationEvent {
-    public ItemStack getItemStack() {
-        return itemStack;
-    }
+	private ItemStack itemStack;
+	private Tier tier;
+	private ItemGenerationReason reason;
 
-    private final ItemStack itemStack;
-    private final Tier tier;
-    private final ItemGenerationReason reason;
+	public RandomItemGenerationEvent(ItemGenerationReason reason, Tier tier, ItemStack itemStack) {
+		this.reason = reason;
+		this.tier = tier;
+		this.itemStack = itemStack;
+	}
 
-    public RandomItemGenerationEvent(ItemGenerationReason reason, Tier tier, ItemStack itemStack) {
-        this.reason = reason;
-        this.tier = tier;
-        this.itemStack = itemStack;
-    }
+	public ItemStack getItemStack() {
+		return itemStack;
+	}
 
-    @Override
-    public ItemGenerationReason getReason() {
-        return reason;
-    }
+	public void setItemStack(ItemStack itemStack) {
+		this.itemStack = itemStack;
+	}
 
-    @Override
-    public Tier getTier() {
-        return tier;
-    }
+	@Override
+	public ItemGenerationReason getReason() {
+		return reason;
+	}
+
+	@Override
+	public Tier getTier() {
+		return tier;
+	}
+
+	public void setTier(Tier tier) {
+		this.tier = tier;
+	}
+
+	public void setReason(ItemGenerationReason reason) {
+		this.reason = reason;
+	}
 }
