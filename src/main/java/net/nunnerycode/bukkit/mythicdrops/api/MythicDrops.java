5f9c7af4c5edcc31c9712ecbcec9f6f8ff97a98b
Richard Harrah <topplethenunnery@gmail.com>
2013-10-29 16:39:04 -0400

    populating MythicDrops interface

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/api/MythicDrops.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/api/MythicDrops.java
@@ -1,4 +1,67 @@
 package net.nunnerycode.bukkit.mythicdrops.api;
 
+import com.conventnunnery.libraries.config.CommentedConventYamlConfiguration;
+import java.io.File;
+import net.nunnerycode.bukkit.mythicdrops.api.commands.MythicCommand;
+import net.nunnerycode.bukkit.mythicdrops.api.loaders.ConfigLoader;
+import net.nunnerycode.bukkit.mythicdrops.api.managers.CustomItemManager;
+import net.nunnerycode.bukkit.mythicdrops.api.managers.DropManager;
+import net.nunnerycode.bukkit.mythicdrops.api.managers.EntityManager;
+import net.nunnerycode.bukkit.mythicdrops.api.managers.ItemManager;
+import net.nunnerycode.bukkit.mythicdrops.api.managers.LanguageManager;
+import net.nunnerycode.bukkit.mythicdrops.api.managers.NameManager;
+import net.nunnerycode.bukkit.mythicdrops.api.managers.SettingsManager;
+import net.nunnerycode.bukkit.mythicdrops.api.managers.TierManager;
+import net.nunnerycode.bukkit.mythicdrops.api.savers.ConfigSaver;
+
 public interface MythicDrops {
+
+	CommentedConventYamlConfiguration getConfigYAML();
+
+	CommentedConventYamlConfiguration getCustomItemsYAML();
+
+	CommentedConventYamlConfiguration getItemGroupsYAML();
+
+	CommentedConventYamlConfiguration getLanguageYAML();
+
+	CommentedConventYamlConfiguration getTierYAML();
+
+	EntityManager getEntityManager();
+
+	DropManager getDropManager();
+
+	ConfigLoader getLanguageLoader();
+
+	ConfigLoader getSettingsLoader();
+
+	ConfigLoader getCustomItemLoader();
+
+	NameManager getNameManager();
+
+	void reload();
+
+	SettingsManager getSettingsManager();
+
+	ConfigLoader getTierLoader();
+
+	TierManager getTierManager();
+
+	LanguageManager getLanguageManager();
+
+	CustomItemManager getCustomItemManager();
+
+	ItemManager getItemManager();
+
+	File getJar();
+
+	ConfigSaver getCustomItemSaver();
+
+	ConfigSaver getLanguageSaver();
+
+	ConfigSaver getTierSaver();
+
+	ConfigSaver getSettingsSaver();
+
+	MythicCommand getCommand();
+
 }
