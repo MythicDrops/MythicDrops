f205dde37f12431230366c33d1289ea349d5f498
Richard Harrah <topplethenunnery@gmail.com>
2013-10-29 14:44:09 -0400

    populating DropManager interface

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/api/managers/DropManager.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/api/managers/DropManager.java
@@ -1,4 +1,22 @@
 package net.nunnerycode.bukkit.mythicdrops.api.managers;
 
+import net.nunnerycode.bukkit.mythicdrops.api.items.CustomItem;
+import net.nunnerycode.bukkit.mythicdrops.api.items.ItemGenerationReason;
+import net.nunnerycode.bukkit.mythicdrops.api.tiers.Tier;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
+
 public interface DropManager {
+
+	ItemStack generateItemStackFromCustomItem(CustomItem customItem, ItemGenerationReason reason);
+
+	ItemStack generateItemStack(ItemGenerationReason reason);
+
+	ItemStack generateItemStackFromTier(Tier tier, ItemGenerationReason reason);
+
+	ItemStack constructItemStackFromTierAndMaterialData(Tier tier, MaterialData materialData,
+														ItemGenerationReason reason);
+
+	ItemStack constructItemStackFromMaterialData(MaterialData matData, ItemGenerationReason reason);
+
 }
