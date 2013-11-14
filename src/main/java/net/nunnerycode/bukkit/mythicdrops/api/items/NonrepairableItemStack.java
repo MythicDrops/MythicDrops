11b9f2436fb7a3e38e7cdcddd4c7e4b40aa700b4
Richard Harrah <topplethenunnery@gmail.com>
2013-11-14 15:16:15 -0500

    adding NonrepairableItemStack class

---
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/api/items/NonrepairableItemStack.java
@@ -0,0 +1,217 @@
+package net.nunnerycode.bukkit.mythicdrops.api.items;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.meta.Repairable;
+
+/**
+ * An ItemStack that cannot be repaired without using commands.
+ */
+public class NonrepairableItemStack extends MythicItemStack {
+
+	public static final int DEFAULT_COST = 1000;
+
+	/**
+	 * Instantiates an ItemStack with a display name.
+	 *
+	 * @param type        material
+	 * @param amount      amount
+	 * @param durability  damage / durability
+	 * @param displayName name of item
+	 * @param cost        experience level cost to repair
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, String displayName, int cost) {
+		this(type, amount, durability, displayName, new ArrayList<String>(), new HashMap<Enchantment, Integer>(),
+				cost);
+	}
+
+	/**
+	 * Instantiates an ItemStack with a display name, lore, and enchantments.
+	 *
+	 * @param type         material
+	 * @param amount       amount
+	 * @param durability   damage / durability
+	 * @param displayName  name of item
+	 * @param lore         lore for item
+	 * @param enchantments enchantments for item
+	 * @param cost         experience level cost to repair
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, String displayName, List<String> lore,
+								  Map<Enchantment, Integer> enchantments, int cost) {
+		super(type, amount, durability, displayName, lore, enchantments);
+		if (this instanceof Repairable) {
+			((Repairable) this).setRepairCost(cost);
+		}
+	}
+
+	/**
+	 * Instantiates an ItemStack with lore.
+	 *
+	 * @param type       material
+	 * @param amount     amount
+	 * @param durability damage / durability
+	 * @param lore       lore for item
+	 * @param cost       experience level cost to repair
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, List<String> lore, int cost) {
+		this(type, amount, durability, null, lore, new HashMap<Enchantment, Integer>(), cost);
+	}
+
+	/**
+	 * Instantiates an ItemStack with enchantments.
+	 *
+	 * @param type         material
+	 * @param amount       amount
+	 * @param durability   damage / durability
+	 * @param enchantments enchantments for item
+	 * @param cost         experience level cost to repair
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, Map<Enchantment,
+			Integer> enchantments, int cost) {
+		this(type, amount, durability, null, new ArrayList<String>(), enchantments, cost);
+	}
+
+	/**
+	 * Instantiates an ItemStack with a display name and lore.
+	 *
+	 * @param type        material
+	 * @param amount      amount
+	 * @param durability  damage / durability
+	 * @param displayName name of item
+	 * @param lore        lore for item
+	 * @param cost        experience level cost to repair
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, String displayName, List<String> lore,
+								  int cost) {
+		this(type, amount, durability, displayName, lore, new HashMap<Enchantment, Integer>(), cost);
+	}
+
+	/**
+	 * Instantiates an ItemStack with a display name and enchantments.
+	 *
+	 * @param type         material
+	 * @param amount       amount
+	 * @param durability   damage / durability
+	 * @param displayName  name of item
+	 * @param enchantments enchantments for item
+	 * @param cost         experience level cost to repair
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, String displayName, Map<Enchantment,
+			Integer> enchantments, int cost) {
+		this(type, amount, durability, displayName, new ArrayList<String>(), enchantments, cost);
+	}
+
+	/**
+	 * Instantiates an ItemStack with lore and enchantments.
+	 *
+	 * @param type         material
+	 * @param amount       amount
+	 * @param durability   damage / durability
+	 * @param lore         lore for item
+	 * @param enchantments enchantments for item
+	 * @param cost         experience level cost to repair
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, List<String> lore, Map<Enchantment,
+			Integer> enchantments, int cost) {
+		this(type, amount, durability, null, lore, enchantments, cost);
+	}
+
+	/**
+	 * Instantiates an ItemStack with a display name.
+	 *
+	 * @param type        material
+	 * @param amount      amount
+	 * @param durability  damage / durability
+	 * @param displayName name of item
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, String displayName) {
+		this(type, amount, durability, displayName, new ArrayList<String>(), new HashMap<Enchantment, Integer>(),
+				DEFAULT_COST);
+	}
+
+	/**
+	 * Instantiates an ItemStack with a display name, lore, and enchantments.
+	 *
+	 * @param type         material
+	 * @param amount       amount
+	 * @param durability   damage / durability
+	 * @param displayName  name of item
+	 * @param lore         lore for item
+	 * @param enchantments enchantments for item
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, String displayName, List<String> lore,
+								  Map<Enchantment, Integer> enchantments) {
+		this(type, amount, durability, displayName, lore, enchantments, DEFAULT_COST);
+	}
+
+	/**
+	 * Instantiates an ItemStack with lore.
+	 *
+	 * @param type       material
+	 * @param amount     amount
+	 * @param durability damage / durability
+	 * @param lore       lore for item
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, List<String> lore) {
+		this(type, amount, durability, null, lore, new HashMap<Enchantment, Integer>(), DEFAULT_COST);
+	}
+
+	/**
+	 * Instantiates an ItemStack with enchantments.
+	 *
+	 * @param type         material
+	 * @param amount       amount
+	 * @param durability   damage / durability
+	 * @param enchantments enchantments for item
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, Map<Enchantment,
+			Integer> enchantments) {
+		this(type, amount, durability, null, new ArrayList<String>(), enchantments, DEFAULT_COST);
+	}
+
+	/**
+	 * Instantiates an ItemStack with a display name and lore.
+	 *
+	 * @param type        material
+	 * @param amount      amount
+	 * @param durability  damage / durability
+	 * @param displayName name of item
+	 * @param lore        lore for item
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, String displayName, List<String> lore) {
+		this(type, amount, durability, displayName, lore, new HashMap<Enchantment, Integer>(), DEFAULT_COST);
+	}
+
+	/**
+	 * Instantiates an ItemStack with a display name and enchantments.
+	 *
+	 * @param type         material
+	 * @param amount       amount
+	 * @param durability   damage / durability
+	 * @param displayName  name of item
+	 * @param enchantments enchantments for item
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, String displayName, Map<Enchantment,
+			Integer> enchantments) {
+		this(type, amount, durability, displayName, new ArrayList<String>(), enchantments, DEFAULT_COST);
+	}
+
+	/**
+	 * Instantiates an ItemStack with lore and enchantments.
+	 *
+	 * @param type         material
+	 * @param amount       amount
+	 * @param durability   damage / durability
+	 * @param lore         lore for item
+	 * @param enchantments enchantments for item
+	 */
+	public NonrepairableItemStack(Material type, int amount, short durability, List<String> lore, Map<Enchantment,
+			Integer> enchantments) {
+		this(type, amount, durability, null, lore, enchantments, DEFAULT_COST);
+	}
+
+}
