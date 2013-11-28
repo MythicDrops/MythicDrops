ef71a23982b21175f0cd5ea463588b725e057d6c
Richard Harrah <topplethenunnery@gmail.com>
2013-11-27 21:47:52 -0500

    adding ItemGenerationReason

---
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/api/items/ItemGenerationReason.java
@@ -0,0 +1,18 @@
+package net.nunnerycode.bukkit.mythicdrops.api.items;
+
+public enum ItemGenerationReason {
+
+	/**
+	 * Whenever mobs spawn
+	 */
+	MONSTER_SPAWN,
+	/**
+	 * Whenever spawned by command
+	 */
+	COMMAND,
+	/**
+	 * For use by external plugins
+	 */
+	EXTERNAL
+
+}
