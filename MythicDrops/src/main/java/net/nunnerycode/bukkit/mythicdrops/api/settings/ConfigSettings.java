74d532790347b7542abf017c59ec135bade5ec89
Richard Harrah <topplethenunnery@gmail.com>
2013-12-01 18:48:54 -0500

    adding ConfigSettings interface

---
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/settings/ConfigSettings.java
@@ -0,0 +1,41 @@
+package net.nunnerycode.bukkit.mythicdrops.api.settings;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ConfigSettings {
+
+	// itemGroups.yml
+	List<String> getArmorTypes();
+
+	List<String> getToolTypes();
+
+	List<String> getMaterialTypes();
+
+	Map<String, List<String>> getItemTypesWithIds();
+
+	Map<String, List<String>> getMaterialTypesWithIds();
+
+	// config.yml
+	boolean isAutoUpdate();
+
+	boolean isDebugMode();
+
+	String getItemDisplayNameFormat();
+
+	boolean isRandomLoreEnabled();
+
+	double getRandomLoreChance();
+
+	List<String> getTooltipFormat();
+
+	// language.yml
+	String getLanguageString(String key);
+
+	String getLanguageString(String key, String[][] args);
+
+	String getFormattedLanguageString(String key);
+
+	String getFormattedLanguageString(String key, String[][] args);
+
+}
