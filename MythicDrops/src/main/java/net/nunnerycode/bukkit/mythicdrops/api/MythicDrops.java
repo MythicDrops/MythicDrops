0ff9fbe8edbda55cc9d6aca6388bc306c913caa1
Richard Harrah <topplethenunnery@gmail.com>
2013-12-01 18:51:05 -0500

    adding some methods to MythicDrops interface

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/MythicDrops.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/MythicDrops.java
@@ -1,4 +1,15 @@
 package net.nunnerycode.bukkit.mythicdrops.api;
 
+import net.nunnerycode.bukkit.mythicdrops.api.items.CustomItemMap;
+import net.nunnerycode.bukkit.mythicdrops.api.items.DropFactory;
+import net.nunnerycode.bukkit.mythicdrops.api.settings.ConfigSettings;
+
 public interface MythicDrops {
+
+	ConfigSettings getConfigSettings();
+
+	DropFactory getDropFactory();
+
+	CustomItemMap getCustomItemMap();
+
 }
