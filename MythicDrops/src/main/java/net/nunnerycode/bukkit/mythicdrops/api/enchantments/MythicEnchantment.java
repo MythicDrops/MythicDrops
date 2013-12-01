33bd93dc592cdb5c9a42e55e24abe924c4b3e50e
Richard Harrah <topplethenunnery@gmail.com>
2013-12-01 18:25:35 -0500

    adding javadoc to MythicEnchantment

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/enchantments/MythicEnchantment.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/enchantments/MythicEnchantment.java
@@ -2,32 +2,76 @@ package net.nunnerycode.bukkit.mythicdrops.api.enchantments;
 
 import org.bukkit.enchantments.Enchantment;
 
+/**
+ * A class containing an {@link Enchantment} and a minimum and maximum level.
+ */
 public class MythicEnchantment {
 
 	private final Enchantment enchantment;
 	private final int minimumLevel;
 	private final int maximumLevel;
 
+	/**
+	 * Instantiate a new MythicEnchantment with an {@link Enchantment} and a minimum and maximum level.
+	 *
+	 * @param enchantment  Enchantment to use
+	 * @param minimumLevel minimum level of Enchantment
+	 * @param maximumLevel maximum level of Enchantment
+	 */
 	public MythicEnchantment(Enchantment enchantment, int minimumLevel, int maximumLevel) {
 		this.enchantment = enchantment;
 		this.minimumLevel = Math.min(minimumLevel, maximumLevel);
 		this.maximumLevel = Math.max(minimumLevel, maximumLevel);
 	}
 
+	/**
+	 * Gets the {@link Enchantment}.
+	 *
+	 * @return Enchantment
+	 */
 	public Enchantment getEnchantment() {
 		return enchantment;
 	}
 
-	public int getMinimumLevel() {
-		return minimumLevel;
+	/**
+	 * Returns the range between the minimum and maximum levels of the Enchantment.
+	 * <br>
+	 * Equivalent of {@code {@link #getMaximumLevel()} - {@link #getMinimumLevel()} }
+	 *
+	 * @return range between the minimum and maximum levels
+	 */
+	public double getRange() {
+		return getMaximumLevel() - getMinimumLevel();
 	}
 
+	/**
+	 * Returns the maximum level of the Enchantment.
+	 *
+	 * @return maximum level
+	 */
 	public int getMaximumLevel() {
 		return maximumLevel;
 	}
 
-	public double getRange() {
-		return getMaximumLevel() - getMinimumLevel();
+	/**
+	 * Returns the minimum level of the Enchantment.
+	 *
+	 * @return minimum level
+	 */
+	public int getMinimumLevel() {
+		return minimumLevel;
+	}
+
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		result = enchantment != null ? enchantment.hashCode() : 0;
+		temp = Double.doubleToLongBits(minimumLevel);
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(maximumLevel);
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
 	}
 
 	@Override
@@ -42,17 +86,5 @@ public class MythicEnchantment {
 		if (enchantment != null ? !enchantment.equals(that.enchantment) : that.enchantment != null) return false;
 
 		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		int result;
-		long temp;
-		result = enchantment != null ? enchantment.hashCode() : 0;
-		temp = Double.doubleToLongBits(minimumLevel);
-		result = 31 * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(maximumLevel);
-		result = 31 * result + (int) (temp ^ (temp >>> 32));
-		return result;
 	}
 }
