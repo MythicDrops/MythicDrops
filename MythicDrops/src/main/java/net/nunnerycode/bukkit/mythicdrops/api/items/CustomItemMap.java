26d03b1c7eae63b6b6c4fb8c3bb36bebe248b92d
Richard Harrah <topplethenunnery@gmail.com>
2013-12-01 20:10:20 -0500

    moving CustomItemMap back

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/items/CustomItemMap.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/items/CustomItemMap.java
@@ -1,17 +1,47 @@
 package net.nunnerycode.bukkit.mythicdrops.api.items;
 
-public interface CustomItemMap {
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.commons.lang.math.RandomUtils;
+
+/**
+ * An extension of {@link ConcurrentHashMap} designed to allow easy developer access to {@link CustomItem}s.
+ */
+public final class CustomItemMap extends ConcurrentHashMap<String, CustomItem> {
+
+	private static final CustomItemMap _INSTANCE = new CustomItemMap();
+
+	private CustomItemMap() {
+		// do nothing
+	}
 
 	/**
-	 * Returns a random {@link CustomItem}.
-	 * @return a random CustomItem
+	 * Gets the instance of CustomItemMap running on the server.
+	 * @return instance running on the server
 	 */
-	public CustomItem getRandom();
+	public static CustomItemMap getInstance() {
+		return _INSTANCE;
+	}
 
-	/**
-	 * Returns a random {@link CustomItem} by using chances of items. Returns null if unable to get an item.
-	 * @return random CustomItem
-	 */
-	public CustomItem getRandomWithChance();
+	public CustomItem getRandom() {
+		CustomItem[] valueArray = values().toArray(new CustomItem[values().size()]);
+		return valueArray[RandomUtils.nextInt(values().size())];
+	}
+
+	public CustomItem getRandomWithChance() {
+		CustomItem[] valueArray = values().toArray(new CustomItem[values().size()]);
+		CustomItem randomCustomItem = null;
+		int zeroSize = 0;
+		while (randomCustomItem == null && zeroSize < valueArray.length) {
+			CustomItem ci = getRandom();
+			if (ci.getChanceToBeGivenToAMonster() <= 0D) {
+				zeroSize++;
+				continue;
+			}
+			if (RandomUtils.nextDouble() < ci.getChanceToBeGivenToAMonster()) {
+				randomCustomItem = ci;
+			}
+		}
+		return randomCustomItem;
+	}
 
 }
