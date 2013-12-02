c5fa72f67c9e16c43fba534bd0ce244a7a3be5fe
Richard Harrah <topplethenunnery@gmail.com>
2013-12-01 19:07:39 -0500

    adding very basic MythicDropFactory

---
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/items/factories/MythicDropFactory.java
@@ -0,0 +1,46 @@
+package net.nunnerycode.bukkit.mythicdrops.items.factories;
+
+import net.nunnerycode.bukkit.mythicdrops.api.items.ItemGenerationReason;
+import net.nunnerycode.bukkit.mythicdrops.api.items.MythicItemStack;
+import net.nunnerycode.bukkit.mythicdrops.api.items.factories.DropFactory;
+import net.nunnerycode.bukkit.mythicdrops.api.tiers.Tier;
+import org.bukkit.Material;
+import org.bukkit.material.MaterialData;
+
+public class MythicDropFactory implements DropFactory {
+
+	private Tier tier;
+	private MaterialData materialData;
+	private ItemGenerationReason itemGenerationReason;
+
+	public MythicDropFactory() {
+		tier = null;
+		materialData = new MaterialData(Material.AIR);
+		itemGenerationReason = ItemGenerationReason.DEFAULT;
+	}
+
+	@Override
+	public DropFactory withTier(Tier tier) {
+		this.tier = tier;
+		return this;
+	}
+
+	@Override
+	public DropFactory withMaterialData(MaterialData materialData) {
+		this.materialData = materialData;
+		return this;
+	}
+
+	@Override
+	public DropFactory withItemGenerationReason(ItemGenerationReason reason) {
+		this.itemGenerationReason = reason;
+		return this;
+	}
+
+	@Override
+	public MythicItemStack build() {
+		// TODO: actually write a build method
+		return null;
+	}
+
+}
