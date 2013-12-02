6c24f3f9d864790894d3af2680d3dfdc406049fe
Richard Harrah <topplethenunnery@gmail.com>
2013-12-02 16:11:14 -0500

    adding NameFactory interface

---
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/names/factories/NameFactory.java
@@ -0,0 +1,18 @@
+package net.nunnerycode.bukkit.mythicdrops.api.names.factories;
+
+import net.nunnerycode.bukkit.mythicdrops.api.items.MythicItemStack;
+import net.nunnerycode.bukkit.mythicdrops.api.tiers.Tier;
+
+public interface NameFactory {
+
+	NameFactory withFormat(String format);
+
+	NameFactory withMythicItemStack(MythicItemStack mis);
+
+	NameFactory withTier(Tier t);
+
+	NameFactory withTier(String tierName);
+
+	String build();
+
+}
