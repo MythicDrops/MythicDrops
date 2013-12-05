672cd2ee5072560bba46958e96baadcbfcf78779
Richard Harrah <topplethenunnery@gmail.com>
2013-12-05 00:40:33 -0500

    adding DebugPrinter to MythicDrops and MythicDropsPlugin

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/MythicDropsPlugin.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/MythicDropsPlugin.java
@@ -2,6 +2,8 @@ package net.nunnerycode.bukkit.mythicdrops;
 
 import net.nunnerycode.bukkit.mythicdrops.api.MythicDrops;
 import net.nunnerycode.bukkit.mythicdrops.api.settings.ConfigSettings;
+import net.nunnerycode.bukkit.mythicdrops.settings.MythicConfigSettings;
+import net.nunnerycode.java.libraries.cannonball.DebugPrinter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MythicDropsPlugin extends JavaPlugin implements MythicDrops {
@@ -9,10 +11,14 @@ public final class MythicDropsPlugin extends JavaPlugin implements MythicDrops {
 	private static MythicDrops _INSTANCE;
 
 	private ConfigSettings configSettings;
+	private DebugPrinter debugPrinter;
 
 	@Override
 	public void onEnable() {
 		_INSTANCE = this;
+
+		debugPrinter = new DebugPrinter(getDataFolder().getPath(), "debug.log");
+		configSettings = new MythicConfigSettings();
 	}
 
 	public static MythicDrops getInstance() {
@@ -22,6 +28,11 @@ public final class MythicDropsPlugin extends JavaPlugin implements MythicDrops {
 	@Override
 	public ConfigSettings getConfigSettings() {
 		return configSettings;
+	}
+
+	@Override
+	public DebugPrinter getDebugPrinter() {
+		return debugPrinter;
 	}
 
 }
--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/MythicDrops.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/MythicDrops.java
@@ -1,9 +1,12 @@
 package net.nunnerycode.bukkit.mythicdrops.api;
 
 import net.nunnerycode.bukkit.mythicdrops.api.settings.ConfigSettings;
+import net.nunnerycode.java.libraries.cannonball.DebugPrinter;
 
 public interface MythicDrops {
 
 	ConfigSettings getConfigSettings();
+
+	DebugPrinter getDebugPrinter();
 
 }
