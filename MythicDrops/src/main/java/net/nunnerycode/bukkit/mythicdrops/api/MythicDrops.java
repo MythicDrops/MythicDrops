022bd466bf8541cbfc5895aa54877f33662dfcfa
Richard Harrah <topplethenunnery@gmail.com>
2013-12-05 00:42:13 -0500

    adding methods to get configuration files to MythicDrops

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/MythicDrops.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/MythicDrops.java
@@ -1,5 +1,6 @@
 package net.nunnerycode.bukkit.mythicdrops.api;
 
+import net.nunnerycode.bukkit.libraries.config.NunneryConfiguration;
 import net.nunnerycode.bukkit.mythicdrops.api.settings.ConfigSettings;
 import net.nunnerycode.java.libraries.cannonball.DebugPrinter;
 
@@ -8,5 +9,15 @@ public interface MythicDrops {
 	ConfigSettings getConfigSettings();
 
 	DebugPrinter getDebugPrinter();
+
+	NunneryConfiguration getConfigYAML();
+
+	NunneryConfiguration getCustomItemYAML();
+
+	NunneryConfiguration getItemGroupsYAML();
+
+	NunneryConfiguration getLanguageYAML();
+
+	NunneryConfiguration getTierYAML();
 
 }
