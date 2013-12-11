6819c7a834f3149a22f538c8fce3095d8fcc2461
Richard Harrah <topplethenunnery@gmail.com>
2013-12-10 19:07:21 -0500

    adding give subcommand

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/commands/MythicDropsCommand.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/commands/MythicDropsCommand.java
@@ -145,5 +145,50 @@ public final class MythicDropsCommand {
 				new String[][]{{"%amount%", String.valueOf(amountGiven)}}));
 	}
 
+	@Command(identifier = "mythicdrops give", description = "Gives MythicDrops items",
+			permissions = "mythicdrops.command.give")
+	@Flags(identifier = {"a", "t", "mind", "maxd"}, description = {"Amount to spawn", "Tier to spawn",
+			"Minimum durability", "Maximum durability"})
+	public void giveSubcommand(CommandSender sender, @Arg(name = "player") Player player, @Arg(name = "amount",
+			def = "1")
+	@FlagArg("a") int amount, @Arg(name = "tier", def = "*") @FlagArg("t") String tierName,
+							   @Arg(name = "mindurability", def = "1.0", verifiers = "min[0.0]|max[1.0]") @FlagArg
+									   ("mind") double minDura,
+							   @Arg(name = "maxdurability", def = "1.0", verifiers = "min[0.0]|max[1.0]") @FlagArg
+									   ("maxd") double maxDura) {
+		if (tierName.equalsIgnoreCase("*") && !sender.hasPermission("mythicdrops.command.give.wildcard")) {
+			sender.sendMessage(plugin.getConfigSettings().getFormattedLanguageString("command.no-access"));
+			return;
+		}
+
+		Tier tier = getTier(tierName, player.getWorld().getName());
+
+		if (!sender.hasPermission("mythicdrops.command.give.wildcard")) {
+			if (tier == null) {
+				sender.sendMessage(plugin.getConfigSettings().getFormattedLanguageString("command" +
+						".tier-does-not-exist"));
+				return;
+			} else if (!player.hasPermission("mythicdrops.command.give." + tier.getName())) {
+				sender.sendMessage(plugin.getConfigSettings().getFormattedLanguageString("command.no-access"));
+				return;
+			}
+		}
+
+		int amountGiven = 0;
+		while (amountGiven < amount) {
+			MythicItemStack mis = new MythicDropBuilder().inWorld(player.getWorld()).useDurability(false)
+					.withItemGenerationReason(ItemGenerationReason.COMMAND).withTier(getTier(tierName,
+							player.getWorld().getName())).build();
+			if (mis != null) {
+				player.getInventory().addItem(mis);
+				amountGiven++;
+			}
+		}
+
+		player.sendMessage(plugin.getConfigSettings().getFormattedLanguageString("command.give-random-receiver",
+				new String[][]{{"%amount%", String.valueOf(amountGiven)}}));
+		sender.sendMessage(plugin.getConfigSettings().getFormattedLanguageString("command.give-random-sender",
+				new String[][]{{"%amount%", String.valueOf(amountGiven)}}));
+	}
 
 }
