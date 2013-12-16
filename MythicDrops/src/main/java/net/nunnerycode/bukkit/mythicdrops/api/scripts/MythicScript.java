9afbe8d53b77213346c1c58b168693f270a7e23f
Richard Harrah <topplethenunnery@gmail.com>
2013-12-16 00:41:48 -0500

    adding MythicScript interface

---
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/api/scripts/MythicScript.java
@@ -0,0 +1,9 @@
+package net.nunnerycode.bukkit.mythicdrops.api.scripts;
+
+import net.nunnerycode.bukkit.mythicdrops.api.MythicDrops;
+
+public interface MythicScript {
+
+	void init(MythicDrops mythicDrops);
+
+}
