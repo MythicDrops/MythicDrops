6497ab0677416c4fd2f9337f8916a05cdd542255
Richard Harrah <topplethenunnery@gmail.com>
2013-12-23 20:34:38 -0500

    loading SocketGems

--- MythicDrops-Sockets/src/main/java/net/nunnerycode/bukkit/mythicdrops/sockets/MythicDropsSockets.java
+++ MythicDrops-Sockets/src/main/java/net/nunnerycode/bukkit/mythicdrops/sockets/MythicDropsSockets.java
@@ -4,9 +4,13 @@ import com.conventnunnery.libraries.config.CommentedConventYamlConfiguration;
 import com.conventnunnery.libraries.config.ConventYamlConfiguration;
 import net.nunnerycode.java.libraries.cannonball.DebugPrinter;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.bukkit.Effect;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffectType;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,6 +38,8 @@ public class MythicDropsSockets extends JavaPlugin {
 	private double socketGemChanceToSpawn;
 	private List<MaterialData> socketGemMaterialIds;
 	private Map<String, SocketGem> socketGemMap;
+	private List<String> socketGemPrefixes;
+	private List<String> socketGemSuffixes;
 
 	public static MythicDropsSockets getInstance() {
 		return _INSTANCE;
@@ -87,6 +93,8 @@ public class MythicDropsSockets extends JavaPlugin {
 		language = new HashMap<>();
 		socketGemMaterialIds = new ArrayList<>();
 		socketGemMap = new HashMap<>();
+		socketGemPrefixes = new ArrayList<>();
+		socketGemSuffixes = new ArrayList<>();
 
 		unpackConfigurationFiles(new String[]{"config.yml", "socketGems.yml"}, false);
 
@@ -108,22 +116,119 @@ public class MythicDropsSockets extends JavaPlugin {
 		debugPrinter.debug(Level.INFO, "v" + getDescription().getVersion() + " enabled");
 	}
 
-	private void unpackConfigurationFiles(String[] configurationFiles, boolean overwrite) {
-		for (String s : configurationFiles) {
-			YamlConfiguration yc = CommentedConventYamlConfiguration.loadConfiguration(getResource(s));
-			try {
-				File f = new File(getDataFolder(), s);
-				if (!f.exists()) {
-					yc.save(f);
-					continue;
-				}
-				if (overwrite) {
-					yc.save(f);
-				}
-			} catch (IOException e) {
-				getLogger().warning("Could not unpack " + s);
+	private void loadGems() {
+		socketGemMap.clear();
+		List<String> loadedSocketGems = new ArrayList<>();
+		for (String key : socketGemsYAML.getKeys(false)) {
+			if (!socketGemsYAML.isConfigurationSection(key)) {
+				continue;
 			}
+			ConfigurationSection gemCS = socketGemsYAML.getConfigurationSection(key);
+			GemType gemType = GemType.getFromName(gemCS.getString("type"));
+			if (gemType == null) {
+				gemType = GemType.ANY;
+			}
+			List<SocketPotionEffect> socketPotionEffects = buildSocketPotionEffects(gemCS);
+			List<SocketParticleEffect> socketParticleEffects = buildSocketParticleEffects(gemCS);
+			double chance = gemCS.getDouble("chance");
+			String prefix = gemCS.getString("prefix");
+			if (prefix != null && !prefix.equalsIgnoreCase("")) {
+				socketGemPrefixes.add(prefix);
+			}
+			String suffix = gemCS.getString("suffix");
+			if (suffix != null && !suffix.equalsIgnoreCase("")) {
+				socketGemSuffixes.add(suffix);
+			}
+			List<String> lore = gemCS.getStringList("lore");
+			Map<Enchantment, Integer> enchantments = new HashMap<>();
+			if (gemCS.isConfigurationSection("enchantments")) {
+				ConfigurationSection enchCS = gemCS.getConfigurationSection("enchantments");
+				for (String key1 : enchCS.getKeys(false)) {
+					Enchantment ench = null;
+					for (Enchantment ec : Enchantment.values()) {
+						if (ec.getName().equalsIgnoreCase(key1)) {
+							ench = ec;
+							break;
+						}
+					}
+					if (ench == null) {
+						continue;
+					}
+					int level = enchCS.getInt(key1);
+					enchantments.put(ench, level);
+				}
+			}
+			List<String> commands = gemCS.getStringList("commands");
+			List<SocketCommand> socketCommands = new ArrayList<>();
+			for (String s : commands) {
+				SocketCommand sc = new SocketCommand(s);
+				socketCommands.add(sc);
+			}
+			SocketGem sg = new SocketGem(key, gemType, socketPotionEffects, socketParticleEffects, chance, prefix,
+					suffix, lore, enchantments, socketCommands);
+			socketGemMap.put(key, sg);
+			loadedSocketGems.add(key);
 		}
+		debugPrinter.debug(Level.INFO, "Loaded socket gems: " + loadedSocketGems.toString());
+	}
+
+	private List<SocketPotionEffect> buildSocketPotionEffects(ConfigurationSection cs) {
+		List<SocketPotionEffect> socketPotionEffectList = new ArrayList<>();
+		if (!cs.isConfigurationSection("potion-effects")) {
+			return socketPotionEffectList;
+		}
+		ConfigurationSection cs1 = cs.getConfigurationSection("potion-effects");
+		for (String key : cs1.getKeys(false)) {
+			PotionEffectType pet = PotionEffectType.getByName(key);
+			if (pet == null) {
+				continue;
+			}
+			int duration = cs1.getInt(key + ".duration");
+			int intensity = cs1.getInt(key + ".intensity");
+			int radius = cs1.getInt(key + ".radius");
+			String target = cs1.getString(key + ".target");
+			EffectTarget et = EffectTarget.getFromName(target);
+			if (et == null) {
+				et = EffectTarget.NONE;
+			}
+			boolean affectsWielder = cs1.getBoolean(key + ".affectsWielder");
+			boolean affectsTarget = cs1.getBoolean(key + ".affectsTarget");
+			socketPotionEffectList.add(new SocketPotionEffect(pet, intensity, duration, radius, et, affectsWielder,
+					affectsTarget));
+		}
+		return socketPotionEffectList;
+	}
+
+	private List<SocketParticleEffect> buildSocketParticleEffects(ConfigurationSection cs) {
+		List<SocketParticleEffect> socketParticleEffectList = new ArrayList<>();
+		if (!cs.isConfigurationSection("particle-effects")) {
+			return socketParticleEffectList;
+		}
+		ConfigurationSection cs1 = cs.getConfigurationSection("particle-effects");
+		for (String key : cs1.getKeys(false)) {
+			Effect pet;
+			try {
+				pet = Effect.valueOf(key);
+			} catch (Exception e) {
+				continue;
+			}
+			if (pet == null) {
+				continue;
+			}
+			int duration = cs1.getInt(key + ".duration");
+			int intensity = cs1.getInt(key + ".intensity");
+			int radius = cs1.getInt(key + ".radius");
+			String target = cs1.getString(key + ".target");
+			EffectTarget et = EffectTarget.getFromName(target);
+			if (et == null) {
+				et = EffectTarget.NONE;
+			}
+			boolean affectsWielder = cs1.getBoolean(key + ".affectsWielder");
+			boolean affectsTarget = cs1.getBoolean(key + ".affectsTarget");
+			socketParticleEffectList.add(new SocketParticleEffect(pet, intensity, duration, radius, et,
+					affectsWielder, affectsTarget));
+		}
+		return socketParticleEffectList;
 	}
 
 	private void loadSettings() {
@@ -165,8 +270,22 @@ public class MythicDropsSockets extends JavaPlugin {
 		}
 	}
 
-	private void loadGems() {
-
+	private void unpackConfigurationFiles(String[] configurationFiles, boolean overwrite) {
+		for (String s : configurationFiles) {
+			YamlConfiguration yc = CommentedConventYamlConfiguration.loadConfiguration(getResource(s));
+			try {
+				File f = new File(getDataFolder(), s);
+				if (!f.exists()) {
+					yc.save(f);
+					continue;
+				}
+				if (overwrite) {
+					yc.save(f);
+				}
+			} catch (IOException e) {
+				getLogger().warning("Could not unpack " + s);
+			}
+		}
 	}
 
 	public String getLanguageString(String key, String[][] args) {
