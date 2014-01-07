95a5defaae27a8cdc163a81066116e066851842c
Richard Harrah <topplethenunnery@gmail.com>
2014-01-07 01:28:53 -0500

    fix for NPE

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/socketting/SocketItem.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/socketting/SocketItem.java
@@ -2,35 +2,17 @@ package net.nunnerycode.bukkit.mythicdrops.socketting;
 
 import net.nunnerycode.bukkit.mythicdrops.MythicDropsPlugin;
 import net.nunnerycode.bukkit.mythicdrops.api.items.MythicItemStack;
+import net.nunnerycode.bukkit.mythicdrops.utils.StringUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.material.MaterialData;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public final class SocketItem extends MythicItemStack {
 
 	public SocketItem(MaterialData materialData, SocketGem socketGem) {
-		super(materialData.getItemType(), 1, (short) 0, ChatColor.GOLD + replaceArgs
-				(MythicDropsPlugin.getInstance().getSockettingSettings().getSocketGemName(), new String[][]{{"%socketgem%",
-						socketGem.getName()}}) + ChatColor.GOLD, replaceArgs(MythicDropsPlugin.getInstance()
+		super(materialData.getItemType(), 1, (short) 0, ChatColor.GOLD + StringUtil.replaceArgs(MythicDropsPlugin
+				.getInstance().getSockettingSettings().getSocketGemName(), new String[][]{{"%socketgem%",
+				socketGem.getName()}}) + ChatColor.GOLD, StringUtil.replaceArgs(MythicDropsPlugin.getInstance()
 				.getSockettingSettings().getSocketGemLore(), new String[][]{{"%type%", socketGem.getPresentableType()}}));
-	}
-
-	private static String replaceArgs(String string, String[][] args) {
-		String s = string;
-		for (String[] arg : args) {
-			s = s.replace(arg[0], arg[1]);
-		}
-		return s;
-	}
-
-	private static List<String> replaceArgs(List<String> strings, String[][] args) {
-		List<String> list = new ArrayList<>();
-		for (String s : strings) {
-			list.add(replaceArgs(s, args));
-		}
-		return list;
 	}
 
 }
