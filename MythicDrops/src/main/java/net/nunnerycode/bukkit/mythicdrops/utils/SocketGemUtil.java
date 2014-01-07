c6722d2414e7889b8c87977aa4c4ddbec90aa83b
Richard Harrah <topplethenunnery@gmail.com>
2014-01-07 00:15:35 -0500

    adding multiple methods for commands

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/utils/SocketGemUtil.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/utils/SocketGemUtil.java
@@ -2,13 +2,18 @@ package net.nunnerycode.bukkit.mythicdrops.utils;
 
 import net.nunnerycode.bukkit.mythicdrops.MythicDropsPlugin;
 import net.nunnerycode.bukkit.mythicdrops.socketting.SocketGem;
+import org.apache.commons.lang.math.RandomUtils;
 import org.apache.commons.lang3.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public final class SocketGemUtil {
 
@@ -33,6 +38,36 @@ public final class SocketGemUtil {
 			}
 		}
 		return null;
+	}
+
+	public static SocketGem getRandomSocketGemWithChance() {
+		Map<String, SocketGem> socketGemMap = MythicDropsPlugin.getInstance().getSockettingSettings().getSocketGemMap
+				();
+		if (socketGemMap == null || socketGemMap.isEmpty()) {
+			return null;
+		}
+		Set<SocketGem> zeroChanceSocketGems = new HashSet<>();
+		while (zeroChanceSocketGems.size() != socketGemMap.size()) {
+			for (SocketGem socket : socketGemMap.values()) {
+				if (socket.getChance() <= 0.0D) {
+					zeroChanceSocketGems.add(socket);
+					continue;
+				}
+				if (RandomUtils.nextDouble() < socket.getChance()) {
+					return socket;
+				}
+			}
+		}
+		return null;
+	}
+
+	public static MaterialData getRandomSocketGemMaterial() {
+		List<MaterialData> materialDatas = MythicDropsPlugin.getInstance().getSockettingSettings()
+				.getSocketGemMaterialDatas();
+		if (materialDatas == null || materialDatas.isEmpty()) {
+			return null;
+		}
+		return materialDatas.get(RandomUtils.nextInt(materialDatas.size()));
 	}
 
 	public static ItemStack addSocket(ItemStack stack) {
