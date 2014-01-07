b5773b8e2557b95300a3350775b3f737b662ea48
Richard Harrah <topplethenunnery@gmail.com>
2014-01-06 20:45:21 -0500

    temporarily preventing compile errors

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/sockets/SocketItem.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/sockets/SocketItem.java
@@ -1,17 +1,20 @@
 package net.nunnerycode.bukkit.mythicdrops.sockets;
 
 import net.nunnerycode.bukkit.mythicdrops.api.items.MythicItemStack;
-import org.bukkit.ChatColor;
-import org.bukkit.material.MaterialData;
+import org.bukkit.Material;
 
 public class SocketItem extends MythicItemStack {
 
-	public SocketItem(MaterialData materialData, SocketGem socketGem) {
-		super(materialData.getItemType(), 1, (short) 0, ChatColor.GOLD + MythicDropsSockets.getInstance().replaceArgs
-				(MythicDropsSockets.getInstance().getSocketGemName(), new String[][]{{"%socketgem%",
-						socketGem.getName()}}) + ChatColor.GOLD, MythicDropsSockets.getInstance().replaceArgs
-				(MythicDropsSockets.getInstance().getSocketGemLore(), new String[][]{{"%type%",
-						socketGem.getPresentableType()}}));
+	public SocketItem(Material type) {
+		super(type);
 	}
+
+//	public SocketItem(MaterialData materialData, SocketGem socketGem) {
+//		super(materialData.getItemType(), 1, (short) 0, ChatColor.GOLD + MythicDropsSockets.getInstance().replaceArgs
+//				(MythicDropsSockets.getInstance().getSocketGemName(), new String[][]{{"%socketgem%",
+//						socketGem.getName()}}) + ChatColor.GOLD, MythicDropsSockets.getInstance().replaceArgs
+//				(MythicDropsSockets.getInstance().getSocketGemLore(), new String[][]{{"%type%",
+//						socketGem.getPresentableType()}}));
+//	}
 
 }
