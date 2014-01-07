d7c5a10a8df18a8e89bf43b78b844279bfd1f826
Richard Harrah <topplethenunnery@gmail.com>
2014-01-06 23:58:05 -0500

    adding SocketGemUtil

---
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/utils/SocketGemUtil.java
@@ -0,0 +1,49 @@
+package net.nunnerycode.bukkit.mythicdrops.utils;
+
+import net.nunnerycode.bukkit.mythicdrops.MythicDropsPlugin;
+import org.apache.commons.lang3.Validate;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SocketGemUtil {
+
+	private SocketGemUtil() {
+		// do nothing;
+	}
+
+	public static ItemStack addSockets(ItemStack stack, int amount) {
+		Validate.notNull(stack, "ItemStack cannot be null");
+
+		ItemStack is = stack;
+		for (int i = 0; i < amount; i++) {
+			is = addSocket(is);
+		}
+		return is;
+	}
+
+	public static ItemStack addSocket(ItemStack stack) {
+		Validate.notNull(stack, "ItemStack cannot be null");
+
+		ItemMeta itemMeta = stack.hasItemMeta() ? stack.getItemMeta() : Bukkit.getItemFactory().getItemMeta(stack.getType());
+		if (itemMeta == null) {
+			return stack;
+		}
+
+		List<String> lore = new ArrayList<>();
+		if (itemMeta.hasLore()) {
+			lore = new ArrayList<>(itemMeta.getLore());
+		}
+
+		lore.add(MythicDropsPlugin.getInstance().getSockettingSettings().getSockettedItemString());
+
+		itemMeta.setLore(lore);
+		stack.setItemMeta(itemMeta);
+
+		return stack;
+	}
+
+}
