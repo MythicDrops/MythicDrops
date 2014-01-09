ccf81c938b3866426038dcea3644def88d819a08
Richard Harrah <topplethenunnery@gmail.com>
2014-01-09 10:09:57 -0500

    using abstract classes over concrete classes

--- MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/socketting/SocketGem.java
+++ MythicDrops/src/main/java/net/nunnerycode/bukkit/mythicdrops/socketting/SocketGem.java
@@ -1,6 +1,7 @@
 package net.nunnerycode.bukkit.mythicdrops.socketting;
 
 import net.nunnerycode.bukkit.mythicdrops.api.socketting.GemType;
+import net.nunnerycode.bukkit.mythicdrops.api.socketting.SocketEffect;
 import org.apache.commons.lang3.text.WordUtils;
 import org.bukkit.enchantments.Enchantment;
 
@@ -11,8 +12,7 @@ public final class SocketGem {
 
 	private final String name;
 	private final GemType gemType;
-	private final List<SocketPotionEffect> socketPotionEffects;
-	private final List<SocketParticleEffect> socketParticleEffects;
+	private final List<SocketEffect> socketEffects;
 	private final double chance;
 	private final String prefix;
 	private final String suffix;
@@ -20,13 +20,12 @@ public final class SocketGem {
 	private final Map<Enchantment, Integer> enchantments;
 	private final List<SocketCommand> commands;
 
-	public SocketGem(String name, GemType gemType, List<SocketPotionEffect> socketPotionEffects,
-					 List<SocketParticleEffect> socketParticleEffects, double chance, String prefix, String suffix,
-					 List<String> lore, Map<Enchantment,  Integer> enchantments, List<SocketCommand> commands) {
+	public SocketGem(String name, GemType gemType, List<SocketEffect> socketEffects, double chance, String prefix,
+					 String suffix, List<String> lore, Map<Enchantment, Integer> enchantments,
+					 List<SocketCommand> commands) {
 		this.name = name;
 		this.gemType = gemType;
-		this.socketPotionEffects = socketPotionEffects;
-		this.socketParticleEffects = socketParticleEffects;
+		this.socketEffects = socketEffects;
 		this.chance = chance;
 		this.prefix = prefix;
 		this.suffix = suffix;
@@ -47,16 +46,16 @@ public final class SocketGem {
 		return name;
 	}
 
-	public GemType getGemType() {
-		return gemType;
-	}
-
 	public String getPresentableType() {
 		return WordUtils.capitalize(getGemType().getName());
 	}
 
-	public List<SocketPotionEffect> getSocketPotionEffects() {
-		return socketPotionEffects;
+	public GemType getGemType() {
+		return gemType;
+	}
+
+	public List<SocketEffect> getSocketEffects() {
+		return socketEffects;
 	}
 
 	public double getChance() {
@@ -73,10 +72,6 @@ public final class SocketGem {
 
 	public List<String> getLore() {
 		return lore;
-	}
-
-	public List<SocketParticleEffect> getSocketParticleEffects() {
-		return socketParticleEffects;
 	}
 
 }
