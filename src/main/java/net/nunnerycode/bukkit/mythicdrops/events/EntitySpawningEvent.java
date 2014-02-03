c33a92d24abd46922462b5c5474356e68d3560e0
Richard Harrah <rmh4209@gmail.com>
2014-02-03 16:24:44 -0500

    EntitySpawningEvent is no longer cancellable

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/events/EntitySpawningEvent.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/events/EntitySpawningEvent.java
@@ -1,10 +1,10 @@
 package net.nunnerycode.bukkit.mythicdrops.events;
 
-import net.nunnerycode.bukkit.mythicdrops.api.events.MythicDropsCancellableEvent;
+import net.nunnerycode.bukkit.mythicdrops.api.events.MythicDropsEvent;
 
 import org.bukkit.entity.LivingEntity;
 
-public class EntitySpawningEvent extends MythicDropsCancellableEvent {
+public class EntitySpawningEvent extends MythicDropsEvent {
 
   private LivingEntity livingEntity;
 
