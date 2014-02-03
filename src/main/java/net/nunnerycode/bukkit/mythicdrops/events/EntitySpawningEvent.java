6d63e24bbc72b492c4874b711a7a5d1a2f9ea5f7
Richard Harrah <rmh4209@gmail.com>
2014-02-03 16:24:02 -0500

    removing useless constructor parameter

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/events/EntitySpawningEvent.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/events/EntitySpawningEvent.java
@@ -3,13 +3,12 @@ package net.nunnerycode.bukkit.mythicdrops.events;
 import net.nunnerycode.bukkit.mythicdrops.api.events.MythicDropsCancellableEvent;
 
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.inventory.ItemStack;
 
 public class EntitySpawningEvent extends MythicDropsCancellableEvent {
 
   private LivingEntity livingEntity;
 
-  public EntitySpawningEvent(LivingEntity livingEntity, ItemStack[] equipment) {
+  public EntitySpawningEvent(LivingEntity livingEntity) {
     this.livingEntity = livingEntity;
   }
 
