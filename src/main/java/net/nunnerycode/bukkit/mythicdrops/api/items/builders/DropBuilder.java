73ac952f5f56158dcf33fd4a29634f1259ce1085
Richard Harrah <rmh4209@gmail.com>
2014-02-08 21:21:07 -0500

    deprecating methods in DropBuilder, adding new method to replace

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/api/items/builders/DropBuilder.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/api/items/builders/DropBuilder.java
@@ -3,6 +3,7 @@ package net.nunnerycode.bukkit.mythicdrops.api.items.builders;
 import net.nunnerycode.bukkit.mythicdrops.api.items.ItemGenerationReason;
 import net.nunnerycode.bukkit.mythicdrops.api.tiers.Tier;
 
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.MaterialData;
@@ -13,8 +14,12 @@ public interface DropBuilder {
 
   DropBuilder withTier(String tierName);
 
+  DropBuilder withMaterial(Material material);
+
+  @Deprecated
   DropBuilder withMaterialData(MaterialData materialData);
 
+  @Deprecated
   DropBuilder withMaterialData(String materialDataString);
 
   DropBuilder withItemGenerationReason(ItemGenerationReason reason);
