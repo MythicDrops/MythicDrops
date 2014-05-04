92322d1797fc5d4aa97170fd14119637aeff5f25
Richard Harrah <topplethenunnery@gmail.com>
2014-05-04 14:30:41 -0400

    adding PopulateWorld parameter to constructor

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/events/ChestPopulateEvent.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/events/ChestPopulateEvent.java
@@ -1,6 +1,7 @@
 package net.nunnerycode.bukkit.mythicdrops.events;
 
 import net.nunnerycode.bukkit.mythicdrops.api.events.MythicDropsCancellableEvent;
+import net.nunnerycode.bukkit.mythicdrops.api.populating.PopulateWorld;
 import org.bukkit.block.Chest;
 import org.bukkit.inventory.ItemStack;
 
@@ -11,10 +12,12 @@ public class ChestPopulateEvent extends MythicDropsCancellableEvent {
 
     private final Chest chest;
     private final List<ItemStack> itemsToAdd;
+    private final PopulateWorld populateWorld;
 
-    public ChestPopulateEvent(Chest chest, List<ItemStack> itemsToAdd) {
+    public ChestPopulateEvent(Chest chest, List<ItemStack> itemsToAdd, PopulateWorld populateWorld) {
         this.chest = chest;
         this.itemsToAdd = itemsToAdd != null ? itemsToAdd : new ArrayList<ItemStack>();
+        this.populateWorld = populateWorld;
     }
 
     public Chest getChest() {
@@ -35,6 +38,10 @@ public class ChestPopulateEvent extends MythicDropsCancellableEvent {
 
     public void clearItems() {
         itemsToAdd.clear();
+    }
+
+    public PopulateWorld getPopulateWorld() {
+        return populateWorld;
     }
 
 }
