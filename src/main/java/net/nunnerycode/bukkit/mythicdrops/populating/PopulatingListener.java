6a28373bd2decfffaed5637bc178bf9ea860bd7d
Richard Harrah <topplethenunnery@gmail.com>
2014-05-04 14:47:36 -0400

    actually populating chests

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/populating/PopulatingListener.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/populating/PopulatingListener.java
@@ -1,12 +1,22 @@
 package net.nunnerycode.bukkit.mythicdrops.populating;
 
 import net.nunnerycode.bukkit.mythicdrops.MythicDropsPlugin;
+import net.nunnerycode.bukkit.mythicdrops.api.items.CustomItem;
+import net.nunnerycode.bukkit.mythicdrops.api.items.ItemGenerationReason;
 import net.nunnerycode.bukkit.mythicdrops.api.populating.PopulateWorld;
 import net.nunnerycode.bukkit.mythicdrops.events.ChestGenerateEvent;
 import net.nunnerycode.bukkit.mythicdrops.events.ChestPopulateEvent;
+import net.nunnerycode.bukkit.mythicdrops.identification.IdentityTome;
+import net.nunnerycode.bukkit.mythicdrops.identification.UnidentifiedItem;
+import net.nunnerycode.bukkit.mythicdrops.items.CustomItemMap;
+import net.nunnerycode.bukkit.mythicdrops.socketting.SocketGem;
+import net.nunnerycode.bukkit.mythicdrops.socketting.SocketItem;
+import net.nunnerycode.bukkit.mythicdrops.utils.RandomRangeUtil;
+import net.nunnerycode.bukkit.mythicdrops.utils.SocketGemUtil;
 import org.apache.commons.lang.math.RandomUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
@@ -17,6 +27,7 @@ import org.bukkit.event.world.ChunkPopulateEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public final class PopulatingListener implements Listener {
 
@@ -62,6 +73,57 @@ public final class PopulatingListener implements Listener {
                 event.getChest().getInventory().addItem(array);
             }
         }, 20L * 1);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onChestPopulateEvent(ChestPopulateEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        int num = RandomRangeUtil.randomRange(event.getPopulateWorld().getMinimumItems(),
+                event.getPopulateWorld().getMaximumItems());
+        List<String> tiers = event.getPopulateWorld().getTiers();
+        if (tiers == null || tiers.isEmpty()) {
+            return;
+        }
+
+        // Begin to check for socket gem, identity tome, and unidentified.
+        double customItemChance = mythicDrops.getConfigSettings().getCustomItemChance();
+        double socketGemChance = mythicDrops.getConfigSettings().getSocketGemChance();
+        double unidentifiedItemChance = mythicDrops.getConfigSettings().getUnidentifiedItemChance();
+        double identityTomeChance = mythicDrops.getConfigSettings().getIdentityTomeChance();
+        boolean sockettingEnabled = mythicDrops.getConfigSettings().isSockettingEnabled();
+        boolean identifyingEnabled = mythicDrops.getConfigSettings().isIdentifyingEnabled();
+
+        for (int i = 0; i < num; i++) {
+            ItemStack itemStack = MythicDropsPlugin.getNewDropBuilder().withItemGenerationReason(
+                    ItemGenerationReason.POPULATING).useDurability(true).withTier(tiers.get(RandomUtils.nextInt
+                    (tiers.size()))).build();
+
+            if (itemStack == null) {
+                continue;
+            }
+
+            if (RandomUtils.nextDouble() <= customItemChance) {
+                CustomItem customItem = CustomItemMap.getInstance().getRandomWithChance();
+                if (customItem != null) {
+                    itemStack = customItem.toItemStack();
+                }
+            } else if (sockettingEnabled && RandomUtils.nextDouble() <= socketGemChance) {
+                SocketGem socketGem = SocketGemUtil.getRandomSocketGemWithChance();
+                Material material = SocketGemUtil.getRandomSocketGemMaterial();
+                if (socketGem != null && material != null) {
+                    itemStack = new SocketItem(material, socketGem);
+                }
+            } else if (identifyingEnabled && RandomUtils.nextDouble() <= unidentifiedItemChance) {
+                Material material = itemStack.getType();
+                itemStack = new UnidentifiedItem(material);
+            } else if (identifyingEnabled && RandomUtils.nextDouble() <= identityTomeChance) {
+                itemStack = new IdentityTome();
+            }
+
+            event.addItem(itemStack);
+        }
     }
 
 }
