d0f1669af94462c7c857a3ec74bf2ca684d2e517
Richard Harrah <topplethenunnery@gmail.com>
2014-07-11 07:19:08 -0400

    adding constructor and cleaning if checks

--- src/main/java/net/nunnerycode/bukkit/mythicdrops/anvil/AnvilListener.java
+++ src/main/java/net/nunnerycode/bukkit/mythicdrops/anvil/AnvilListener.java
@@ -1,12 +1,13 @@
 package net.nunnerycode.bukkit.mythicdrops.anvil;
 
-import net.nunnerycode.bukkit.mythicdrops.MythicDropsPlugin;
+import net.nunnerycode.bukkit.mythicdrops.api.MythicDrops;
 import net.nunnerycode.bukkit.mythicdrops.api.tiers.Tier;
 import net.nunnerycode.bukkit.mythicdrops.socketting.SocketGem;
 import net.nunnerycode.bukkit.mythicdrops.utils.SocketGemUtil;
 import net.nunnerycode.bukkit.mythicdrops.utils.TierUtil;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -17,26 +18,37 @@ import org.bukkit.inventory.ItemStack;
 
 public final class AnvilListener implements Listener {
 
+    private final MythicDrops mythicDrops;
+
+    public AnvilListener(MythicDrops mythicDrops) {
+        this.mythicDrops = mythicDrops;
+    }
+
     @EventHandler(priority = EventPriority.MONITOR)
     public void onItemRename(InventoryClickEvent e) {
-        if (!e.isCancelled() && MythicDropsPlugin.getInstance().getConfigSettings().isRepairingEnabled()) {
-            HumanEntity ent = e.getWhoClicked();
-            if (ent instanceof Player) {
-                Inventory inv = e.getInventory();
-                if (inv instanceof AnvilInventory) {
-                    ItemStack fis = inv.getItem(0);
-                    ItemStack sis = inv.getItem(1);
-                    Tier ft = fis != null ? TierUtil.getTierFromItemStack(fis) : null;
-                    Tier st = sis != null ? TierUtil.getTierFromItemStack(sis) : null;
-                    SocketGem fsg = fis != null ? SocketGemUtil.getSocketGemFromItemStack(fis) : null;
-                    SocketGem stg = sis != null ? SocketGemUtil.getSocketGemFromItemStack(sis) : null;
-                    if (ft != null || st != null || fsg != null || stg != null) {
-                        if (e.getSlot() == 2) {
-                            e.setCancelled(true);
-                        }
-                    }
-                }
-            }
+        if (e.isCancelled()) {
+            return;
+        }
+        if (!mythicDrops.getConfigSettings().isRepairingEnabled()) {
+            return;
+        }
+        HumanEntity ent = e.getWhoClicked();
+        if (!(ent instanceof Player)) {
+            return;
+        }
+        Inventory inv = e.getInventory();
+        if (!(inv instanceof AnvilInventory)) {
+            return;
+        }
+        ItemStack fis = inv.getItem(0);
+        ItemStack sis = inv.getItem(1);
+        Tier ft = fis != null ? TierUtil.getTierFromItemStack(fis) : null;
+        Tier st = sis != null ? TierUtil.getTierFromItemStack(sis) : null;
+        SocketGem fsg = fis != null ? SocketGemUtil.getSocketGemFromItemStack(fis) : null;
+        SocketGem stg = sis != null ? SocketGemUtil.getSocketGemFromItemStack(sis) : null;
+        if ((ft != null || st != null || fsg != null || stg != null) && e.getSlot() == 2) {
+            e.setCancelled(true);
+            e.setResult(Event.Result.DENY);
         }
     }
 
