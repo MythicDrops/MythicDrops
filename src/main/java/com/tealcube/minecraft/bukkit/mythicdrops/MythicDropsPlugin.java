79c098038a1de2514d019564e45d3fbf4ae92917
Richard Harrah <topplethenunnery@gmail.com>
2019-07-23 00:12:30 -0400

    fix missing package declaration in MythicDropsPlugin

--- src/main/java/com/tealcube/minecraft/bukkit/mythicdrops/MythicDropsPlugin.java
+++ src/main/java/com/tealcube/minecraft/bukkit/mythicdrops/MythicDropsPlugin.java
@@ -20,8 +20,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+package com.tealcube.minecraft.bukkit.mythicdrops;
+
 import com.modcrafting.diablodrops.name.NamesLoader;
-import com.tealcube.minecraft.bukkit.mythicdrops.MythicDropsPluginExtensionsKt;
 import com.tealcube.minecraft.bukkit.mythicdrops.anvil.AnvilListener;
 import com.tealcube.minecraft.bukkit.mythicdrops.api.MythicDrops;
 import com.tealcube.minecraft.bukkit.mythicdrops.api.enchantments.MythicEnchantment;
@@ -82,30 +83,6 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-
-:/*
- * The MIT License
- * Copyright © 2013 Richard Harrah
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-        package com.tealcube.minecraft.bukkit.mythicdrops;
 
 public final class MythicDropsPlugin extends JavaPlugin implements MythicDrops {
 
