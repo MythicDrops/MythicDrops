a5157ca88e985ffe1c1ba186969a404f71340d7e
Richard Harrah <topplethenunnery@gmail.com>
2019-09-01 00:27:30 -0400

    fixing broken builds caused by repositories not being right

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,16 +10,16 @@ gradle.allprojects {
             url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
         }
         maven {
-            url = uri("https://papermc.io/repo/repository/maven-snapshots")
+            url = uri("https://papermc.io/repo/repository/maven-public")
         }
         maven {
-            url = uri("http://maven.sk89q.com/artifactory/libs-release-local")
+            url = uri("http://maven.sk89q.com/artifactory/repo")
         }
         maven {
             url = uri("https://repo.codemc.org/repository/maven-public")
         }
         maven {
-            url = uri("https://repo.aikar.co/nexus/content/repositories/aikar-snapshots")
+            url = uri("https://repo.aikar.co/content/groups/aikar")
         }
         maven {
             url = uri("https://jitpack.io")
