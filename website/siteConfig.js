555020a3b174aa90a9a305106e69bd9a0f92a7e1
Richard Harrah <topplethenunnery@gmail.com>
2019-09-15 22:56:48 -0400

    clean up of some publishing info

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,20 +9,14 @@
 // site configuration options.
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://pixeloutlaw.github.io', // Your website URL
-  baseUrl: '/MythicDrops/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  title: 'MythicDrops',
+  tagline: 'Action RPG Drops for Minecraft.',
 
+  url: 'https://pixeloutlaw.github.io',
+  baseUrl: '/MythicDrops/',
   // Used for publishing and more
   projectName: 'MythicDrops',
   organizationName: 'PixelOutlaw',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
