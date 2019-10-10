5e3de5e91d2cba656d7592d3e34c68c67ecf05ff
Richard Harrah <topplethenunnery@gmail.com>
2019-10-09 23:13:44 -0400

    switch to docusaurus v2

---
+++ website/docusaurus.config.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+    title: 'MythicDrops',
+    tagline: 'Action RPG style drops in Spigot',
+    url: 'https://pixeloutlaw.github.io',
+    baseUrl: '/MythicDrops/',
+    favicon: 'img/favicon.ico',
+    organizationName: 'PixelOutlaw', // Usually your GitHub org/user name.
+    projectName: 'MythicDrops', // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: 'MythicDrops',
+            logo: {
+                alt: 'MythicDrops Logo',
+                src: 'img/logo.svg',
+            },
+            links: [
+                {to: 'docs/doc1', label: 'Docs', position: 'left'},
+                {to: 'support', label: 'Support', position: 'left'},
+                {
+                    href: 'https://github.com/PixelOutlaw/MythicDrops',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            logo: {
+                alt: 'Facebook Open Source Logo',
+                src: 'https://docusaurus.io/img/oss_logo.png',
+            },
+            copyright: `Copyright © ${new Date().getFullYear()} Richard Harrah. Built with Docusaurus.`,
+        },
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    path: '../docs',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
+    ],
+};
