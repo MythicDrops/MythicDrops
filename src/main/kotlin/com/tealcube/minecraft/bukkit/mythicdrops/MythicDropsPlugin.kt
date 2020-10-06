5a703d9aa1246418f95275a4d273c7efb899f7c0
Richard Harrah <topplethenunnery@gmail.com>
2020-10-05 22:26:58 -0400

    disabling legacy item checks also disables color combo check

--- src/main/kotlin/com/tealcube/minecraft/bukkit/mythicdrops/MythicDropsPlugin.kt
+++ src/main/kotlin/com/tealcube/minecraft/bukkit/mythicdrops/MythicDropsPlugin.kt
@@ -115,11 +115,6 @@ import io.pixeloutlaw.minecraft.spigot.bandsaw.PluginFileHandler
 import io.pixeloutlaw.minecraft.spigot.bandsaw.rebelliousAddHandler
 import io.pixeloutlaw.minecraft.spigot.config.SmartYamlConfiguration
 import io.pixeloutlaw.mythicdrops.mythicdrops.BuildConfig
-import java.io.File
-import java.util.Random
-import java.util.logging.Handler
-import java.util.logging.Level
-import java.util.logging.Logger
 import org.bstats.bukkit.Metrics
 import org.bukkit.Bukkit
 import org.bukkit.enchantments.Enchantment
@@ -138,6 +133,11 @@ import org.bukkit.plugin.java.annotation.plugin.Plugin
 import org.bukkit.plugin.java.annotation.plugin.author.Author
 import org.bukkit.plugin.java.annotation.plugin.author.Authors
 import org.bukkit.scheduler.BukkitTask
+import java.io.File
+import java.util.Random
+import java.util.logging.Handler
+import java.util.logging.Level
+import java.util.logging.Logger
 
 @Plugin(name = BuildConfig.NAME, version = BuildConfig.VERSION)
 @Authors(Author("ToppleTheNun"), Author("pur3p0w3r"))
@@ -820,7 +820,7 @@ class MythicDropsPlugin : JavaPlugin(), MythicDrops {
 
             // check if tier already exists with same color combination
             val preExistingTierWithColors = tierManager.getWithColors(tier.displayColor, tier.identifierColor)
-            if (preExistingTierWithColors != null) {
+            if (preExistingTierWithColors != null && !settingsManager.configSettings.options.isDisableLegacyItemChecks) {
                 val message =
                     "Not loading $key as there is already a tier with that display color and " +
                         "identifier color loaded: ${preExistingTierWithColors.name}"
