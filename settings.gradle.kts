a1024a981d8ce0ec464db997fd87a924f839d679
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2022-11-29 21:21:39 -0500

    update dependencies and gradle (#684)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,7 @@ pluginManagement {
 
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.40.1"
-////                            # available:"0.40.2"
+    id("de.fayard.refreshVersions") version "0.51.0"
 }
 
 gradle.allprojects {
