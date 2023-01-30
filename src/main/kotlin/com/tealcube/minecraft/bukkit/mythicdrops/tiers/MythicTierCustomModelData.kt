f3b5d1a0eae11f51dd6a733c37adfc5e53f5a7f6
Richard Harrah <topplethenunnery@gmail.com>
2023-01-30 13:28:17 -0500

    fix loading incorrect materials
    closes #702

--- src/main/kotlin/com/tealcube/minecraft/bukkit/mythicdrops/tiers/MythicTierCustomModelData.kt
+++ src/main/kotlin/com/tealcube/minecraft/bukkit/mythicdrops/tiers/MythicTierCustomModelData.kt
@@ -1,6 +1,9 @@
 package com.tealcube.minecraft.bukkit.mythicdrops.tiers
 
 import com.tealcube.minecraft.bukkit.mythicdrops.api.tiers.TierCustomModelData
+import dev.mythicdrops.Either
+import dev.mythicdrops.Either.Companion.left
+import dev.mythicdrops.Either.Companion.right
 import io.pixeloutlaw.minecraft.spigot.mythicdrops.enumValueOrNull
 import io.pixeloutlaw.minecraft.spigot.mythicdrops.getOrDefaultAsDefaultValueType
 import org.bukkit.Material
@@ -11,11 +14,19 @@ internal data class MythicTierCustomModelData(
     override val material: Material? = null
 ) : TierCustomModelData {
     companion object {
-        fun fromMap(map: Map<String, Any>): MythicTierCustomModelData {
-            return MythicTierCustomModelData(
-                modelData = map.getOrDefaultAsDefaultValueType("model-data", 0),
-                weight = map.getOrDefaultAsDefaultValueType("weight", 0.0 as Number).toDouble(),
-                material = enumValueOrNull<Material>(map.getOrDefaultAsDefaultValueType("material", ""))
+        fun fromMap(
+            map: Map<String, Any>
+        ): Either<String, MythicTierCustomModelData> {
+            val material = enumValueOrNull<Material>(map.getOrDefaultAsDefaultValueType("material", ""))
+            if (map.containsKey("material") && material == null) {
+                return left("material was present and invalid: ${map["material"]}")
+            }
+            return right(
+                MythicTierCustomModelData(
+                    modelData = map.getOrDefaultAsDefaultValueType("model-data", 0),
+                    weight = map.getOrDefaultAsDefaultValueType("weight", 0.0 as Number).toDouble(),
+                    material = enumValueOrNull<Material>(map.getOrDefaultAsDefaultValueType("material", ""))
+                )
             )
         }
     }
