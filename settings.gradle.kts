40e98cffc9e69b24afe093f0fb8259275ad65f86
Richard Harrah <topplethenunnery@gmail.com>
2023-04-14 16:34:31 -0400

    update dependencies
    closes #714

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 plugins {
     // See https://jmfayard.github.io/refreshVersions
     id("de.fayard.refreshVersions") version "0.51.0"
-    id("com.gradle.enterprise") version "3.12.6"
+    id("com.gradle.enterprise") version "3.13"
 }
 
 gradleEnterprise {
