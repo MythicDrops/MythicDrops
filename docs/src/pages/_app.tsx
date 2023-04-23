11ad5e81161d69680790f48ec4bb4cf925df123e
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2023-04-22 20:48:08 -0400

    add vercel analytics to docs (#717)

--- docs/src/pages/_app.tsx
+++ docs/src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import "../style.css";
 
 const MythicDropsDocs = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 );
 
 export default MythicDropsDocs;
