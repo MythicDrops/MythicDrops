ba75e671664fca1ee47aba0c28031f55ae3a4dc8
Richard Harrah <topplethenunnery@gmail.com>
2023-09-04 20:25:30 -0400

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
 
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.60.1"
+    id("de.fayard.refreshVersions") version "0.60.2"
     id("com.gradle.enterprise") version "3.14.1"
 }
 
