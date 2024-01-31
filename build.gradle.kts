c8788e8ee0b2389ecf0f4c297c8378e35a9fa37e
Richard Harrah <topplethenunnery@gmail.com>
2024-01-30 22:10:23 -0500

    update to gradle 8.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.9.22"
     id("org.jetbrains.kotlinx.binary-compatibility-validator")
     id("dev.mythicdrops.gradle.project")
     id("com.github.johnrengelman.shadow")
